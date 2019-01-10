be151098f0ad024b27bd6468b34c00ed605e79d4
Teague Henry <trhenry@live.unc.edu>
2019-01-10 11:47:52 -0500

    Initial Commit
    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clpipe',
+      version='0.1',
+      description='Cohen Lab MRI processing pipeline',
+      url = 'blank',
+      author = 'Cohen Lab',
+      author_email='cohenlab@email.unc.edu',
+      license='MIT',
+      packages=['clpipe'],
+      zip_safe=False)
