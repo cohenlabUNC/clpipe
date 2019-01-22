e0a963ee87c5e9c590fca0a8592338470f4394fb
Teague Henry <trhenry@live.unc.edu>
2019-01-22 12:34:52 -0500

    Added interactive flag to bids validator

--- clpipe/bids_validator.py
+++ clpipe/bids_validator.py
@@ -8,8 +8,8 @@ from .config_json_parser import ConfigParser
 @click.command()
 @click.option('-configFile', type=click.Path(exists=True, dir_okay=False, file_okay=True), default=None)
 @click.argument('bidsDir', type=click.Path(exists=True, dir_okay=True, file_okay=False))
-
-def bids_validate(bidsdir = None, configfile = None):
+@click.option('-interactive/-batch', default = False )
+def bids_validate(bidsdir = None, configfile = None, interactive = False):
     config = ConfigParser()
     config.config_updater(configfile)
     config.setup_directories(bidsdir, None, None)
@@ -17,13 +17,21 @@ def bids_validate(bidsdir = None, configfile = None):
     batch_manager = BatchManager(batchsystemConfig=config.config['BatchConfig'])
     batch_manager.update_mem_usage('3000')
 
-    singularity_string = '''singularity run --cleanenv -B /proj {validatorInstance} {bidsDir}'''
+    singularity_string = '''singularity run --cleanenv -B {bindPaths} {validatorInstance} {bidsDir}'''
 
-    batch_manager.addjob(Job("BIDSValidator",singularity_string.format(
-        validatorInstance = config.config['BIDSValidatorImage'],
-        bidsDir = config.config['BIDSDirectory']
-    )))
+    if(interactive):
+        os.system(singularity_string.format(
+            validatorInstance = config.config['BIDSValidatorImage'],
+            bidsDir = config.config['BIDSDirectory'],
+            bindPaths = batch_manager.config['SingularityBindPaths']
+        ))
+    else:
+        batch_manager.addjob(Job("BIDSValidator",singularity_string.format(
+            validatorInstance = config.config['BIDSValidatorImage'],
+            bidsDir = config.config['BIDSDirectory'],
+            bindPaths = batch_manager.config['SingularityBindPaths']
+        )))
 
-    batch_manager.compilejobstrings()
-    batch_manager.submit_jobs()
+        batch_manager.compilejobstrings()
+        batch_manager.submit_jobs()
 
