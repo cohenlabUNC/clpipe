4ba35a08c79db49689a7cd4dafdf2e6a9feccd1a
Teague Henry <trhenry@live.unc.edu>
2019-01-29 13:11:07 -0500

    Update __init__.py

--- clpipe/__init__.py
+++ clpipe/__init__.py
@@ -1,3 +1,5 @@
 from clpipe import batch_manager
 from clpipe import config_json_parser
 from clpipe import fmri_preprocess
+from clpipe.postprocutils import utils
+from clpipe.postprocutils import spec_interpolate
