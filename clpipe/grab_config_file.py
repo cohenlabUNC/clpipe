7d43b5c9d555b6bb667ae7cbad47be08dc75be1e
Teague Henry <trhenry@live.unc.edu>
2019-02-06 16:53:32 -0500

    Update grab_config_file.py

--- clpipe/grab_config_file.py
+++ clpipe/grab_config_file.py
@@ -4,9 +4,9 @@ import logging
 import os
 
 @click.command()
-@click.option('-outputName', default = 'AConfigFile.json')
-def grab_config_file(outputname=None):
+@click.option('-outputFile', default = 'AConfigFile.json')
+def grab_config_file(outputfile=None):
     logging.basicConfig(level=logging.INFO)
     config = ConfigParser()
-    config.config_json_dump('.', outputname)
-    logging.info('Config File at '+ os.path.join(os.path.abspath('.'), outputname))
+    config.config_json_dump(os.path.dirname(outputfile), os.path.basename(outputfile))
+    logging.info('Config File at '+ os.path.join(os.path.abspath(os.path.dirname(outputfile)), os.path.basename(outputfile)))
