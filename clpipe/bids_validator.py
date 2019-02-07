29bb345122a5c7c74a10df0738bbca202a2f8fea
Teague Henry <trhenry@live.unc.edu>
2019-02-07 11:50:30 -0500

    Added verbose option to bids_validate
    closes #18

--- clpipe/bids_validator.py
+++ clpipe/bids_validator.py
@@ -1,29 +1,31 @@
 import os
-
+import sys
 import click
-
+import logging
 from .batch_manager import BatchManager, Job
 from .config_json_parser import ConfigParser
 
 @click.command()
 @click.option('-configFile', type=click.Path(exists=True, dir_okay=False, file_okay=True), default=None)
 @click.argument('bidsDir', type=click.Path(exists=True, dir_okay=True, file_okay=False), required= False)
-@click.option('-interactive/-batch', default = False )
+@click.argument('-verbose/-simple', default=False)
+@click.option('-interactive/-batch', default = False)
 @click.option('-submit/-save', default = False)
-def bids_validate(bidsdir = None, configfile = None, interactive = False, submit = True):
+def bids_validate(bidsdir = None, configfile = None, interactive = False, submit = True, verbose=False):
     config = ConfigParser()
     config.config_updater(configfile)
     config.setup_fmriprep_directories(bidsdir, None, None)
     if bidsdir is None and configfile is None:
-        raise ValueError('Specify a BIDS directory in either the configuration file, or in the command')
-
-    if not os.path.isdir(config.config['FMRIPrepOptions']['BIDSDirectory']):
-        raise ValueError('BIDS directory is not a directory, did you specify one?')
+        logging.error('Specify a BIDS directory in either the configuration file, or in the command')
+        sys.exit(1)
 
     batch_manager = BatchManager(batchsystemConfig=config.config['BatchConfig'])
     batch_manager.update_mem_usage('3000')
 
     singularity_string = '''singularity run --cleanenv -B {bindPaths} {validatorInstance} {bidsDir}'''
+
+    if verbose:
+        singularity_string = singularity_string + ' --verbose'
 
     if(interactive):
         os.system(singularity_string.format(
