d54f4c45586ea52385b4b62bd990d91367dbed9b
Kelly D <kellyduffy23@gmail.com>
2019-02-18 13:34:27 -0500

    Updates with now using config file

--- clpipe/dicom_to_bids_converter.py
+++ clpipe/dicom_to_bids_converter.py
@@ -1,16 +1,18 @@
 import click
 import json
 from .batch_manager import BatchManager,Job
+from .config_json_parser import ConfigParser
 
 @click.command()
 @click.argument('subject', nargs=1, required=True, default=None)
 @click.argument('session', nargs = 1, required=True, default=None)
+@click.option('-configFile', type=click.Path(exists=True, dir_okay=False, file_okay=True), default=None)
 @click.argument('dicomdirectory', type=click.Path(exists=True, dir_okay=True, file_okay=False))
 @click.argument('outputfile', type=click.Path(exists=True, dir_okay=False, file_okay=True), default = None)
 @click.option('-logOutputDir', type=click.Path(exists=True, dir_okay=True, file_okay=False), default = None)
 @click.option('-batchConfig', type=click.Path(dir_okay=False, file_okay=True), default = "slurmUNCConfigHeudiconv.json")
 @click.option('-submit/-save', default=False)
-def dicom_to_nifti_to_bids_converter_setup(subject = None, session = None, dicomdirectory=None, outputfile=None,
+def dicom_to_nifti_to_bids_converter_setup(subject = None, session = None, configfile = None, dicomdirectory=None, outputfile=None,
                                            logoutputdir = None, batchconfig = None, submit=False):
 
     #TODO: This function should run heudiconv with a default heuristic file
@@ -21,8 +23,9 @@ def dicom_to_nifti_to_bids_converter_setup(subject = None, session = None, dicom
     # To submit batch commmands, use the BatchManager class
 
     #Do I need to update config file with date ran for just this? Or validate after? It doesn't seem any of this is going in
-    #config = ConfigParser()
-    #config.config_updater(configfile)
+    config = ConfigParser()
+    config.config_updater(configfile)
+    #config.validate_config()
 
     heudiconv_string = '''module add heudiconv \n heudiconv -d {dicomdirectory}/sub-{subject}/ses-{sess}/* -s {subject} '''\
     ''' -ss {sess} -f convertall -o {dicomdirectory}/test/ -b --minmeta \n cp {dicomdirectory}/test/ '''\
@@ -31,6 +34,8 @@ def dicom_to_nifti_to_bids_converter_setup(subject = None, session = None, dicom
     #I have seen other examples of people using other converters, but for now I think we can get rid of it
 
     batch_manager = BatchManager(batchconfig,logoutputdir)
+    batch_manager.update_mem_usage(config.config['HeudiconvOptions']['HeudiconvMemoryUsage'])
+    batch_manager.update_time(config.config['HeudiconvOptions']['HeudiconvTimeUsage'])
     job1 = Job("heudiconv_setup", heudiconv_string.format(
         dicomdirectory=dicomdirectory,
         subject=subject,
