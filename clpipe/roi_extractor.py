a24a346436c0f759a3749a27aaf3ffd2d30152cb
Teague Henry <trhenry@live.unc.edu>
2019-03-10 13:28:40 -0400

    Update roi_extractor.py

--- clpipe/roi_extractor.py
+++ clpipe/roi_extractor.py
@@ -197,6 +197,8 @@ def _fmri_roi_extract_subject(subject, task, atlas_name, atlas_filename, atlas_l
 
 def _fmri_roi_extract_image(data, atlas_path, atlas_type, radius):
     logging.debug(atlas_type)
+    logging.debug(atlas_type is 'sphere')
+    logging.debug(type(atlas_type))
     if atlas_type is 'label':
         logging.debug('Labels Extract')
         label_masker = NiftiLabelsMasker(atlas_path)
