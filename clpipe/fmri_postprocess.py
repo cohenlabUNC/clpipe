7ede96b5d6163df7199c78360a91e3e22d786597
Teague Henry <trhenry@live.unc.edu>
2019-03-13 13:35:18 -0400

    Update fmri_postprocess.py

--- clpipe/fmri_postprocess.py
+++ clpipe/fmri_postprocess.py
@@ -329,6 +329,7 @@ def _beta_series_calc(data, filt_ev_mat, filt_confound_mat):
         logging.debug((numpy.sum(filt_ev_mat,1) - filt_ev_mat[:,index]).shape)
         temp_mat = numpy.concatenate([numpy.expand_dims(filt_ev_mat[:,index],1), numpy.expand_dims(numpy.sum(filt_ev_mat,1) - filt_ev_mat[:,index],1), filt_confound_mat],1)
         temp_beta = numpy.linalg.pinv(temp_mat)
+        logging.debug(temp_beta.shape)
         beta_maker[:, index] = temp_beta[index,:]
 
     betas = numpy.matmul(beta_maker, data)
