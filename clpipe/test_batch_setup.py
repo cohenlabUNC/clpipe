5c8b195a1b27742419a80cb2eed62da42c95c15e
Teague Henry <trhenry@live.unc.edu>
2019-08-13 18:08:18 -0400

    Update test_batch_setup.py

--- clpipe/test_batch_setup.py
+++ clpipe/test_batch_setup.py
@@ -22,7 +22,7 @@ def test_batch_setup(config_file = None, batch_config = None, log_dir = None, su
     batch_manager = BatchManager(config.config['BatchConfig'], os.path.abspath(log_dir))
     batch_manager.update_email(config.config["EmailAddress"])
     os.makedirs(os.path.abspath(log_dir),   exist_ok=True)
-    submission_string = 'python3 -c \\\"print(\\\\\\\"Hello Cluster\\\\\")\\\"'
+    submission_string = 'python3 -c \\\"print(\\\\\\\"Hello Cluster\\\\\\\")\\\"'
     test_IDs = ["Test-" + str(i) for i in range(10)]
 
     for ID in test_IDs:
