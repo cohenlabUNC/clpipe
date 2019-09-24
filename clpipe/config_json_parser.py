9f4e5de3e6e2b170a95fe79b2b76b29e81a9c19f
Teague Henry <trhenry@live.unc.edu>
2019-09-24 13:58:59 -0400

    Update config_json_parser.py

--- clpipe/config_json_parser.py
+++ clpipe/config_json_parser.py
@@ -199,7 +199,7 @@ class ConfigParser:
 
         index = [ind  for ind,e in enumerate(self.config['ProcessingStreams']) if e['ProcessingStream'] == stream_name][0]
         if output_dir is not None:
-            self.config['ProcessingStreams'][index][target_output]['OutputDir'] = os.path.abspath(output_dir)
+            self.config['ProcessingStreams'][index][target_output]['OutputDirectory'] = os.path.abspath(output_dir)
             os.makedirs(self.config['ProcessingStreams'][index][target_output]['OutputDir'], exist_ok=True)
         if output_suffix is not None:
             self.config['ProcessingStreams'][index][target_output]['OutputSuffix'] = output_suffix
