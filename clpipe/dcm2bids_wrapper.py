823ec58ec68a71b689358327b778d29a3c67a30d
Teague Henry <trhenry@live.unc.edu>
2020-01-27 12:26:34 -0500

    Fix merge issue

--- clpipe/dcm2bids_wrapper.py
+++ clpipe/dcm2bids_wrapper.py
@@ -19,12 +19,11 @@ import sys
 @click.option('-log_dir', help = 'Where to put the log files. Defaults to Batch_Output in the current working directory.')
 @click.option('-subject', required = False, help = 'A subject  to convert using the supplied configuration file.  Use to convert single subjects, else leave empty')
 @click.option('-session', required = False, help = 'A session  to convert using the supplied configuration file.  Use in combination with -subject to convert single subject/sessions, else leave empty')
+@click.option('-longitudinal', is_flag = True, default = False, help = 'Convert all subjects/sessions into individual pseudo-subjects. Use if you do not want T1w averaged across sessions during FMRIprep')
 @click.option('-submit', is_flag=True, default=False, help = 'Submit jobs to HPC')
-<<<<<<< HEAD
-def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_config_file = None, config_file = None, overwrite = None, log_dir = None, subject =None, session = None, submit = None):
-    config = ClpipeConfigParser()
-=======
->>>>>>> 6d49ea476e638e30e546360426b5436e0533b004
+
+def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_config_file = None, config_file = None, overwrite = None, log_dir = None, subject =None, session = None, longitudinal = False, submit = None):
+    config = ConfigParser()
     config.config_updater(config_file)
     config.setup_dcm2bids(dicom_dir,
                           conv_config_file,
@@ -69,6 +68,7 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_co
         sys.excepthook = exception_handler
         raise FileNotFoundError('There are no subjects/sessions found for that format string.')
 
+    if session_toggle and not longitudinal:
         conv_string = '''dcm2bids -d {dicom_dir} -o {bids_dir} -p {subject} -s {session} -c {conv_config_file}'''
     else:
         conv_string = '''dcm2bids -d {dicom_dir} -o {bids_dir} -p {subject} -c {conv_config_file}'''
@@ -83,6 +83,7 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_co
     batch_manager.update_nthreads(config.config['DICOMToBIDSOptions']['CoreUsage'])
     for ind,i in enumerate(sub_sess_list):
 
+        if session_toggle and not longitudinal:
              job_id = 'convert_sub-' + i['subject'] + '_ses-' + i['session']
              job1 = Job(job_id, conv_string.format(
                 dicom_dir=folders[ind],
@@ -90,6 +91,14 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir = None, conv_co
                 session =i['session'],
                 conv_config_file = config.config['DICOMToBIDSOptions']['ConversionConfig'],
                 bids_dir = config.config['DICOMToBIDSOptions']['BIDSDirectory']
+            ))
+        elif longitudinal:
+            job_id = 'convert_sub-' + i['subject']+ '_ses-' + i['session']
+            job1 = Job(job_id, conv_string.format(
+                dicom_dir=folders[ind],
+                subject=i['subject'] + "sess"+ i['session'],
+                conv_config_file=config.config['DICOMToBIDSOptions']['ConversionConfig'],
+                bids_dir=config.config['DICOMToBIDSOptions']['BIDSDirectory']
             ))
         else:
             job_id = 'convert_sub-' + i['subject']
