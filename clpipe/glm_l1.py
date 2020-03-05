a5e8b8ebeeff4dfc65f63707d20fee5a5ca34d9a
Teague Henry <trhenry@live.unc.edu>
2020-03-05 12:45:28 -0500

    Update glm_l1.py

--- clpipe/glm_l1.py
+++ clpipe/glm_l1.py
@@ -20,7 +20,7 @@ def glm_l1_preparefsf(glm_config_file, l1_name, debug):
         logging.basicConfig(level=logging.DEBUG)
     glm_config = GLMConfigParser(glm_config_file)
 
-    l1_block = [x for x in glm_config.config['Level1Setups'] if x['ModelName'] is str(l1_name)]
+    l1_block = [x for x in glm_config.config['Level1Setups'] if x['ModelName'] == str(l1_name)]
     logging.debug(type(l1_name))
     logging.debug(type(glm_config.config['Level1Setups'][0]['ModelName']))
     logging.debug(glm_config.config['Level1Setups'][0]['ModelName'] is l1_name)
