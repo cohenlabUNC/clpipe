b3ff1964f88e3e291d1baee6d31e795dc5b4cd14
Teague Henry <trhenry@live.unc.edu>
2020-10-21 14:33:25 -0400

    Update fsl_onset_extract.py

--- clpipe/fsl_onset_extract.py
+++ clpipe/fsl_onset_extract.py
@@ -46,15 +46,16 @@ def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
             temp = data.loc[data[glm_config.config["Level1Onsets"]["TrialTypeVariable"]] == trial_type]
             if len(temp) == 0:
                 logging.warning("Trial Type: "+ trial_type + ", has no entries. If this is unexpected, check your GLM config file for misspellings/incorrect entries in the ['Level1Onsets']['TrialTypeToExtract']")
-            onsets = temp['onset']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
-            duration = temp['duration']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
-            p_response  = numpy.repeat(np.array([1]), len(onsets))
-            if glm_config.config["Level1Onsets"]["ParametricResponseVariable"] != "":
-                p_response = temp[glm_config.config["Level1Onsets"]["ParametricResponseVariable"]]
+            else:
+                onsets = temp['onset']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
+                duration = temp['duration']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
+                p_response  = numpy.repeat(np.array([1]), len(onsets))
+                if glm_config.config["Level1Onsets"]["ParametricResponseVariable"] != "":
+                    p_response = temp[glm_config.config["Level1Onsets"]["ParametricResponseVariable"]]
 
-            to_file = np.array([onsets, duration, p_response])
-            file_name = os.path.basename(file).replace(glm_config.config["Level1Onsets"]['EventFileSuffix'], trial_type + ".txt")
-            output = os.path.join(glm_config.config["Level1Onsets"]['EVDirectory'], file_name)
-            logging.debug(output)
-            numpy.savetxt(output, np.transpose(to_file), "%8.4f")
+                to_file = np.array([onsets, duration, p_response])
+                file_name = os.path.basename(file).replace(glm_config.config["Level1Onsets"]['EventFileSuffix'], trial_type + ".txt")
+                output = os.path.join(glm_config.config["Level1Onsets"]['EVDirectory'], file_name)
+                logging.debug(output)
+                numpy.savetxt(output, np.transpose(to_file), "%8.4f")
 
