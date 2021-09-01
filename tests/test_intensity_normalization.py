4d6b505652f036b1aacb50f60355c015175755bc
Will Asciutto <wjasciutto@gmail.com>
2021-09-01 10:54:25 -0400

    Add test for intensity normalization.

---
+++ tests/test_intensity_normalization.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('../clpipe')
+
+from clpipe.intensity_normalization import intensity_normalization
+from testing_tools import setup_test_directory
+from pathlib import Path
+
+
+def test_intensity_normalization(tmpdir: Path):
+    intensity_normalization(config_file=tmpdir / "clpipe_config.json",
+                            target_dir=tmpdir / "data_postproc",
+                            output_dir=tmpdir / "data_postproc/intensity_normalized",
+                            output_suffix=tmpdir / "normalized",
+                            log_dir=tmpdir / "logs/intensity_normaliztion",
+                            submit=False,
+                            batch=False,
+                            debug=True)
+
+def test_intensity_normalization_None():
+    intensity_normalization(config_file=None,
+                            target_dir=None,
+                            output_dir=None,
+                            output_suffix=None,
+                            log_dir=None,
+                            submit=False,
+                            batch=False,
+                            debug=True)
+
+if __name__ == "__main__":
+    tmpdir = setup_test_directory()
+    test_intensity_normalization(tmpdir)
