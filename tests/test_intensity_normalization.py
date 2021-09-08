d348c3559352a18bf2a8d855f475b5faef2b3418
William John Asciutto <willasc@longleaf-login1.its.unc.edu>
2021-09-08 11:52:40 -0400

    Add fixture for fmriprep output folders. Remove now unnecessary testing_tools module.

--- tests/test_intensity_normalization.py
+++ tests/test_intensity_normalization.py
@@ -1,31 +1,30 @@
 import sys
 import pytest
-sys.path.append('../clpipe')
-
-from click.testing import CliRunner
 from pathlib import Path
 
-from clpipe.intensity_normalization import *
+from click.testing import CliRunner
 
-#tmpdir = setup_test_directory(temporary=True, populate_fmriprep=True)
+sys.path.append('../clpipe')
+from clpipe.intensity_normalization import *
+from clpipe.project_setup import project_setup
 
 CONFIG_FILE_PATH = "clpipe_config.json"
 TARGET_DIR_PATH = "data_fmriprep"
 OUTPUT_DIR_PATH = "data_postproc/postproc_normalize"
 OUTPUT_SUFFIX = "_normalized"
 LOG_DIR_PATH = "logs"
+PROJECT_TITLE = "test_project"
+NUM_SUBJECTS = 8
 
 logging.basicConfig(level=logging.INFO)
 
-PROJECT_TITLE = "test_project"
-from click.testing import CliRunner
-from clpipe.project_setup import project_setup
-
-@pytest.fixture
-def clpipe_dir(tmpdir):
+@pytest.fixture(scope="session")
+def clpipe_dir(tmpdir_factory):
     #TODO: abstract this out for use in future test modules
     """Fixture which provides a temporary clpipe project folder."""
-    raw_data = Path(tmpdir / "data_DICOMs")
+    proj_path = tmpdir_factory.mktemp(PROJECT_TITLE)
+    
+    raw_data = Path(proj_path / "data_DICOMs")
     raw_data.mkdir(parents=True, exist_ok=True)
 
     # Use the clpipe CLI to setup project
@@ -34,7 +33,7 @@ def clpipe_dir(tmpdir):
         project_setup, 
         [
             '-project_title', PROJECT_TITLE, 
-            '-project_dir', str(tmpdir),
+            '-project_dir', str(proj_path),
             '-source_data', str(raw_data), 
         ]
     )
@@ -43,8 +42,26 @@ def clpipe_dir(tmpdir):
     if result.exit_code != 0:
         raise Exception(result.exception)
 
-    return tmpdir
+    return proj_path
 
+@pytest.fixture
+def clpipe_bids_dir(clpipe_dir):
+    """Fixture which adds some subject folders to data_BIDS."""
+
+    for sub_num in range(NUM_SUBJECTS):
+            subject_folder = clpipe_dir / "data_BIDS" / f"sub-{sub_num}"
+            subject_folder.mkdir()
+
+    return clpipe_dir
+
+@pytest.fixture
+def clpipe_fmriprep_dir(clpipe_dir):
+    """Fixture which adds fmriprep subject folders to data_fmriprep."""
+    for sub_num in range(NUM_SUBJECTS):
+        subject_folder = clpipe_dir / "data_fmriprep" / f"sub-{sub_num}"
+        subject_folder.mkdir()
+    
+    return clpipe_dir
 
 @pytest.mark.skip(reason="Not yet implemented")
 def test_intensity_normalization_cli_10000_global_median():
@@ -129,9 +146,9 @@ def test_calculate_10000_global_median():
     image = None
     calculate_10000_global_median(image)
 
-def test_calculate_100_voxel_mean(clpipe_dir):
+def test_calculate_100_voxel_mean(clpipe_fmriprep_dir):
     image = None
-    calculate_100_voxel_mean(str(clpipe_dir), str(clpipe_dir))
+    calculate_100_voxel_mean(str(clpipe_fmriprep_dir), str(clpipe_fmriprep_dir))
 
 if __name__ == "__main__":
     test_calculate_100_voxel_mean()
