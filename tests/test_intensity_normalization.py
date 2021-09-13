088308e3d3cb741285db6fbc5d9b798997c4c1cd
Will Asciutto <wasciutto@unc.edu>
2021-09-13 13:44:34 -0400

    Adjust inputs.

--- tests/test_intensity_normalization.py
+++ tests/test_intensity_normalization.py
@@ -11,9 +11,9 @@ from clpipe.intensity_normalization import *
 from clpipe.project_setup import project_setup
 
 CONFIG_FILE_PATH = "clpipe_config.json"
-TARGET_DIR_PATH = "data_fmriprep"
-OUTPUT_DIR_PATH = "data_postproc/postproc_normalize"
-OUTPUT_SUFFIX = "_normalized"
+FMRI_PREPPED_SUFFIX = "_boldref"
+
+
 LOG_DIR_PATH = "logs"
 PROJECT_TITLE = "test_project"
 NUM_SUBJECTS = 8
@@ -59,10 +59,15 @@ def clpipe_bids_dir(clpipe_dir):
 
 @pytest.fixture
 def clpipe_fmriprep_dir(clpipe_dir):
-    """Fixture which adds fmriprep subject folders to data_fmriprep."""
+    """Fixture which adds fmriprep subject folders and data to data_fmriprep."""
+    fmriprep_postfix = "_boldref"
+
     for sub_num in range(NUM_SUBJECTS):
         subject_folder = clpipe_dir / "data_fmriprep" / f"sub-{sub_num}"
         subject_folder.mkdir()
+
+        nii = generate_random_nii()
+        nib.save(nii, subject_folder / f"sub-{sub_num}_{fmriprep_postfix}.nii")
     
     return clpipe_dir
 
@@ -164,19 +169,20 @@ def test_intensity_normalization_None():
 def test_intensity_normalization_10000_global_median():
     intensity_normalization(rescaling_method=RESCALING_10000_GLOBALMEDIAN,
                             target_dir=TARGET_DIR_PATH,
-                            median_intensity=None,
-                            rescaling_factor=None,
                             output_dir=OUTPUT_DIR_PATH,
                             )
 
 @pytest.mark.skip(reason="Not yet implemented")
-def test_intensity_normalization_100_voxel_mean():
-    intensity_normalization(rescaling_method=RESCALING_100_VOXELMEAN,
-                            target_dir=TARGET_DIR_PATH,
-                            median_intensity=None,
-                            rescaling_factor=None,
-                            output_dir=OUTPUT_DIR_PATH,
+def test_intensity_normalization_100_voxel_mean(clpipe_fmriprep_dir):
+    normalization_path = clpipe_fmriprep_dir / OUTPUT_DIR_PATH
+    
+    intensity_normalization(subjects=[1],
+                            rescaling_method=RESCALING_100_VOXELMEAN,
+                            target_dir=clpipe_fmriprep_dir,
+                            output_dir=normalization_path,
                             )
+
+    sample_path = normalization_path / "sub-1"
 
 def test_calculate_10000_global_median(tmp_path, random_nii):
     out_path = tmp_path / "normalized.nii.gz"
