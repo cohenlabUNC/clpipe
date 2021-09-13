30c84be77a7ea8c0a2adfbb99e5f05df16e8f181
Will Asciutto <wasciutto@unc.edu>
2021-09-13 13:39:17 -0400

    Adjust parameters.

--- clpipe/intensity_normalization.py
+++ clpipe/intensity_normalization.py
@@ -7,6 +7,7 @@ import logging
 import sys
 from .error_handler import exception_handler
 from .utils import parse_dir_subjects, build_arg_string
+from pathlib import Path
 
 from nipype.interfaces.fsl.maths import MeanImage, BinaryMaths, MedianImage
 from nipype.interfaces.fsl.utils import ImageStats
@@ -18,8 +19,9 @@ RESCALING_METHODS = (RESCALING_10000_GLOBALMEDIAN, RESCALING_100_VOXELMEAN)
 RESCALING_DEFAULT = RESCALING_10000_GLOBALMEDIAN
 
 #TODO - should be replaced by configuration parser
-DEFAULT_TARGET_DIR = ""
-DEFAULT_OUTPUT_DIR = ""
+DEFAULT_TARGET_DIR = Path("data_fmriprep")
+DEFAULT_OUTPUT_DIR = Path("data_postproc" / "postproc_normalize")
+DEFAULT_NORMALIZATION_SUFFIX = "_normalized"
 
 LOG = logging.getLogger(__name__)
 
@@ -27,8 +29,6 @@ LOG = logging.getLogger(__name__)
 @click.argument('subjects', nargs=-1, required=False, default=None)
 #TODO: Flesh out the help messages for new args
 @click.option('-rescaling_method', default=RESCALING_DEFAULT)
-@click.option('-median_intensity', default=None)
-@click.option('-rescaling_factor', default=None)
 @click.option('-config_file', type=click.Path(exists=True, dir_okay=False, file_okay=True), default=None,
               help='Use a given configuration file. If left blank, uses the default config file, requiring definition of BIDS, working and output directories.')
 @click.option('-target_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False),
@@ -57,8 +57,8 @@ def intensity_normalization_cli(subjects=None, config_file=None, rescaling_metho
         log_dir=log_dir, submit=submit, batch=batch, debug=debug)
     
 
-def intensity_normalization(subjects:list=None, config_file:str=None, rescaling_method:str=RESCALING_DEFAULT, median_intensity=None, 
-                            rescaling_factor=None, target_dir:str=DEFAULT_TARGET_DIR, target_suffix=None,
+def intensity_normalization(subjects:list=None, config_file:str=None, rescaling_method:str=RESCALING_DEFAULT,
+                            target_dir:str=DEFAULT_TARGET_DIR, target_suffix=None,
                             output_dir:str=DEFAULT_OUTPUT_DIR, output_suffix=None, log_dir=None, submit=False,
                             batch=True, debug=False):
     """The controller for intensity normalization - handles configuration, input scrubbing, file I/O and method selection.
@@ -67,9 +67,6 @@ def intensity_normalization(subjects:list=None, config_file:str=None, rescaling_
         subjects (list, optional): A list of subjects to process.
         config_file (str, optional): A path to a clpipe configuration file to override the project default.
         rescaling_method (str, optional): The method of normalization to perform. Defaults to RESCALING_DEFAULT.
-        TODO: Finish these descriptions
-        median_intensity ([type], optional): [description]. Defaults to None.
-        rescaling_factor ([type], optional): [description]. Defaults to None.
         target_dir (str, optional): A path to the directory of images to normalize. Defaults to DEFAULT_TARGET_DIR.
         target_suffix (str, optional): [description]. Defaults to None.
         output_dir (str): A path to the output directory of normalized images.
@@ -85,8 +82,8 @@ def intensity_normalization(subjects:list=None, config_file:str=None, rescaling_
 
     if debug: LOG.setLevel(logging.DEBUG)
     
-    LOG.debug(build_arg_string(subjects=subjects, config_file=config_file, rescaling_method=rescaling_method,
-        median_intensity=median_intensity, rescaling_factor=rescaling_factor, target_dir=target_dir,
+    LOG.debug(build_arg_string(subjects=subjects, config_file=config_file, 
+        rescaling_method=rescaling_method, target_dir=target_dir,
         target_suffix=target_suffix, output_dir=output_dir, output_suffix=output_suffix,
         log_dir=log_dir, submit=submit, batch=batch, debug=debug))
     
@@ -109,13 +106,10 @@ def intensity_normalization(subjects:list=None, config_file:str=None, rescaling_
         LOG.info(f"Rescaling target: {target_dir}")
         LOG.info(f"Rescaling method: {rescaling_method}")
 
-        # TODO: Ingest image here
-        nii_image = None
-
         if rescaling_method == RESCALING_10000_GLOBALMEDIAN:
-            normalized_nii = calculate_10000_global_median(nii_image, median_intensity=median_intensity, rescaling_factor=rescaling_factor)
+            calculate_10000_global_median(target_dir, output_dir)
         elif rescaling_method == RESCALING_100_VOXELMEAN:
-            normalized_nii = calculate_100_voxel_mean(nii_image)
+            calculate_100_voxel_mean(target_dir, output_dir)
         else:
             raise ValueError(f"Invalid rescaling method: {rescaling_method}")
 
