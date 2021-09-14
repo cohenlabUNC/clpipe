47cd4d860c8c5fb3ed42180fd7916c5cd9f464ce
Will Asciutto <wasciutto@unc.edu>
2021-09-14 17:27:33 -0400

    Add setup for intensity normalization.

--- clpipe/config_json_parser.py
+++ clpipe/config_json_parser.py
@@ -86,6 +86,11 @@ class ClpipeConfigParser:
                             output_dir=os.path.join(self.config['ProjectDirectory'], 'data_postproc',
                                                     'postproc_default'),
                             output_suffix='postproc_default.nii.gz')
+        self.setup_intensity_normalization(os.path.join(self.config['FMRIPrepOptions']['OutputDirectory']),
+                                            None,
+                                            os.path.join(self.config['ProjectDirectory'], 'data_postproc', 'normalized'),
+                                            None,
+                                            None)
         processing_streams = self.get_processing_stream_names()
         if processing_streams:
             for stream in processing_streams:
