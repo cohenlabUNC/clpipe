717781d11dc8158b8872f6394da0c76cad5f17e8
Will Asciutto <wasciutto@unc.edu>
2021-09-21 14:52:53 -0400

    Update test path expectations.

--- tests/test_intensity_normalization.py
+++ tests/test_intensity_normalization.py
@@ -10,12 +10,25 @@ import nibabel as nib
 sys.path.append('../clpipe')
 from clpipe.intensity_normalization import *
 from clpipe.config_json_parser import ClpipeConfigParser
+from conftest import NUM_SUBJECTS
 
 CONFIG_FILE_PATH = "clpipe_config.json"
 FMRI_PREPPED_SUFFIX = "_boldref"
 LOG_DIR_PATH = "logs"
 
 logging.basicConfig(level=logging.INFO)
+
+@pytest.fixture()
+def clpipe_normalized_dir(clpipe_fmriprep_dir):
+    """Fixture which adds normalization output folders"""
+
+    for sub_num in range(NUM_SUBJECTS):
+        voxel_mean_folder = clpipe_fmriprep_dir / "data_normalized" / "100_voxel_mean" / f"sub-{sub_num}"
+        voxel_mean_folder.mkdir(parents=True)
+        global_median_folder = clpipe_fmriprep_dir / "data_normalized" / "10000_global_median" / f"sub-{sub_num}"
+        global_median_folder.mkdir(parents=True)
+    
+    return clpipe_fmriprep_dir
 
 @pytest.mark.skip(reason="Not yet implemented")
 def test_intensity_normalization_cli_10000_global_median():
@@ -85,19 +98,18 @@ def test_intensity_normalization_10000_global_median():
                             config_file=clpipe_fmriprep_dir / "clpipe_config.json"
                             )
 
-def test_normalize_subject_100_voxel_mean(clpipe_fmriprep_dir):
+def test_normalize_subject_100_voxel_mean(clpipe_normalized_dir):
     """Asserts that intensity_normalization() creates a normalized image using the 100 voxel mean method
     and saves the output using the configurations provided in clpipe_config.json """
     
-    subject_path: Path = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep" / "sub-0"
-    output_dir: Path = clpipe_fmriprep_dir / "data_postproc" / "normalized"
+    subject_path: Path = clpipe_normalized_dir / "data_fmriprep" / "fmriprep" / "sub-0"
+    output_dir: Path = clpipe_normalized_dir / "data_normalized" / "100_voxel_mean" / "sub-0"
 
     normalize_subject(subject_path, output_dir,
                         method=calculate_100_voxel_mean)
 
-    expected_path = clpipe_fmriprep_dir \
-        / "data_postproc" / "normalized" \
-        / "sub-0" / "sub-0_task-rest_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold_normalized"
+    expected_path = output_dir \
+        / "sub-0_task-rest_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold_normalized.nii.gz"
 
     assert expected_path.exists(), f"Expected path {expected_path} not found."
 
