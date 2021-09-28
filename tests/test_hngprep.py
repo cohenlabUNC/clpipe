d6266f1275b48b58837554104ff6e7d14ceb0a09
Will Asciutto <wasciutto@unc.edu>
2021-09-28 11:00:29 -0400

    Split out tests for hngprep and intensity normalization, apply necessary refactors.

---
+++ tests/test_hngprep.py
@@ -0,0 +1,105 @@
+import sys
+import pytest
+import logging
+
+from pathlib import Path
+from click.testing import CliRunner
+
+sys.path.append('../clpipe')
+from clpipe.hngprep import hngprep, hngprep_cli, hngprep_subject
+from clpipe.config_json_parser import ClpipeConfigParser
+from conftest import NUM_SUBJECTS
+
+CONFIG_FILE_PATH = "clpipe_config.json"
+FMRI_PREPPED_SUFFIX = "_boldref"
+LOG_DIR_PATH = "logs"
+
+logging.basicConfig(level=logging.INFO)
+
+@pytest.fixture(scope="module")
+def clpipe_hngout_dir(clpipe_fmriprep_dir):
+    """Fixture which adds an hngprep output folder"""
+
+    normalized_folder = clpipe_fmriprep_dir / "data_hngprep"
+    normalized_folder.mkdir()
+    
+    return clpipe_fmriprep_dir
+
+@pytest.fixture(scope="module")
+def hngprep_config(clpipe_config_default, clpipe_hngout_dir):
+    """Fixture which sets up an intensity normalization enabled configuration object."""
+    target_dir: Path = clpipe_hngout_dir / "data_fmriprep" / "fmriprep"
+    output_dir: Path = clpipe_hngout_dir / "data_hngprep"
+
+    clpipe_config_default.config['HNGPrepOptions']['TargetDirectory'] = str(target_dir)
+    clpipe_config_default.config['HNGPrepOptions']['OutputDirectory'] = str(output_dir)
+    clpipe_config_default.config['HNGPrepOptions']['TargetSuffix'] = "preproc_bold.nii.gz"
+    clpipe_config_default.config['HNGPrepOptions']['OutputSuffix'] = "hngprep_bold.nii.gz"
+    clpipe_config_default.config['HNGPrepOptions']['IntensityNormalizationMethod'] = "10000_globalmedian"
+    clpipe_config_default.config['HNGPrepOptions']['LogDirectory'] = \
+        clpipe_hngout_dir / "logs" / "hngprep_logs"
+
+    return clpipe_config_default
+
+def test_hngprep_cli(hngprep_config, clpipe_fmriprep_dir):
+    runner = CliRunner()
+    result = runner.invoke(
+    hngprep_cli, 
+    [
+        '-normalization_method', "10000_globalmedian", 
+        '-config_file', clpipe_fmriprep_dir / "clpipe_config.json",
+        '-target_dir', hngprep_config.config['HNGPrepOptions']['TargetDirectory'],
+        '-output_dir', hngprep_config.config['HNGPrepOptions']['OutputDirectory'], 
+        '-output_suffix', hngprep_config.config['HNGPrepOptions']['OutputSuffix'], 
+        '-log_dir', LOG_DIR_PATH,
+        '-submit', True, 
+        '-batch', False, 
+        '-debug', True, 
+    ])
+
+    assert result.exit_code == 0
+
+def test_hngprep_cli_None():
+    runner = CliRunner()
+    result = runner.invoke(
+    hngprep_cli, 
+    [
+        '-normalization_method', "", 
+        '-config_file', "",
+        '-target_dir', "",
+        '-output_dir', "", 
+        '-output_suffix', "", 
+        '-log_dir', "",
+        '-submit', False, 
+        '-batch', False, 
+        '-debug', True, 
+    ])
+
+    assert result.exit_code != 0
+
+def test_hngprep(hngprep_config, clpipe_fmriprep_dir):
+    """Must create data_normalization folder."""
+    hngprep(subjects=list(range(0, 8)),
+            target_dir=hngprep_config.config['HNGPrepOptions']['TargetDirectory'],
+            output_dir=hngprep_config.config['HNGPrepOptions']['OutputDirectory'],
+            output_suffix=hngprep_config.config['HNGPrepOptions']['OutputSuffix'],
+            config_file=clpipe_fmriprep_dir / "clpipe_config.json",
+            normalization_method="100_voxelmean")
+
+    count = 0
+    for subject in (Path(hngprep_config.config['HNGPrepOptions']['OutputDirectory']) / "data_hngprep").iterdir():
+        count += 1
+
+    assert count == 8
+
+def test_hngprep_subject(hngprep_config):
+    """Asserts that intensity_normalization() creates a normalized image using the 10000 global median method
+    and saves the output using the configurations provided in the configuration object. """
+    hngprep_subject(hngprep_config, 'sub-0')
+
+    expected_path = Path(hngprep_config.config['HNGPrepOptions']['OutputDirectory']) \
+        / "TBD" \
+        / "sub-0" \
+        / "sub-0_task-rest_run-1_space-MNI152NLin2009cAsym_desc-hngprep_bold.nii.gz"
+
+    assert expected_path.exists(), f"Expected path {expected_path} not found."
