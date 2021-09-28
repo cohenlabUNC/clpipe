40dcc1b76a7cdc05f14c41da812d8767b78b301c
Will Asciutto <wasciutto@unc.edu>
2021-09-28 10:11:47 -0400

    Convert intensity_normalization module to a general HNG prep module.

--- clpipe/hngprep.py
+++ clpipe/hngprep.py
@@ -17,14 +17,13 @@ import nipype.pipeline.engine as pe
 
 RESCALING_10000_GLOBALMEDIAN = "10000_globalmedian"
 RESCALING_100_VOXELMEAN = "100_voxelmean"
-METHODS = (RESCALING_10000_GLOBALMEDIAN, RESCALING_100_VOXELMEAN)
+NORMALIZATION_METHODS = (RESCALING_10000_GLOBALMEDIAN, RESCALING_100_VOXELMEAN)
 
 LOG = logging.getLogger(__name__)
 
 @click.command()
 @click.argument('subjects', nargs=-1, required=False, default=None)
 #TODO: Flesh out the help messages for new args
-@click.option('-method', default=None)
 @click.option('-config_file', type=click.Path(exists=True, dir_okay=False, file_okay=True), default=None,
               help='Use a given configuration file. If left blank, uses the default config file, requiring definition of BIDS, working and output directories.')
 @click.option('-target_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False),
@@ -33,8 +32,7 @@ LOG = logging.getLogger(__name__)
               help='Which file suffix to use. If a configuration file is provided with a target suffix, this argument is not necessary. Defaults to "preproc_bold.nii.gz"')
 @click.option('-output_dir', type=click.Path(dir_okay=True, file_okay=False),
               help='Where to put the normalized data. If a configuration file is provided with a output directory, this argument is not necessary.')
-@click.option('-output_suffix',
-              help='What suffix to append to the normalized files. If a configuration file is provided with a output suffix, this argument is not necessary.')
+@click.option('-output_suffix')
 @click.option('-log_dir', type=click.Path(dir_okay=True, file_okay=False),
               help='Where to put HPC output files. If not specified, defaults to <outputDir>/batchOutput.')
 @click.option('-submit', is_flag=True, default=False, help='Flag to submit commands to the HPC.')
@@ -42,26 +40,26 @@ LOG = logging.getLogger(__name__)
               help='Submit to batch, or run in current session. Mainly used internally.')
 @click.option('-debug', is_flag=True, default=False,
               help='Print detailed processing information and traceback for errors.')
-def intensity_normalization_cli(subjects=None, config_file=None, rescaling_method=None, median_intensity=None, 
-                                rescaling_factor=None, target_dir=None, target_suffix=None, output_dir=None,
-                                output_suffix=None, log_dir=None, submit=False, batch=False, debug=False):
+@click.option('-normalization_method', default=None)
+def hngprep_cli(subjects=None, config_file=None, target_dir=None, target_suffix=None, output_dir=None,
+                output_suffix=None, log_dir=None, submit=False, batch=False, debug=False,
+                normalization_method=None,):
 
-    intensity_normalization(subjects=subjects, config_file=config_file, rescaling_method=rescaling_method,
-        median_intensity=median_intensity, rescaling_factor=rescaling_factor, target_dir=target_dir,
-        target_suffix=target_suffix, output_dir=output_dir, output_suffix=output_suffix,
-        log_dir=log_dir, submit=submit, batch=batch, debug=debug)
+    hngprep(subjects=subjects, config_file=config_file, target_dir=target_dir, target_suffix=target_suffix, output_dir=output_dir,
+            output_suffix=output_suffix, log_dir=log_dir, submit=submit, batch=batch, debug=debug,
+            normalization_method=normalization_method)
     
 
-def intensity_normalization(subjects:list=None, config_file:str=None, method:str=None,
-                            target_dir:str=None, target_suffix=None,
-                            output_dir:str=None, output_suffix=None, log_dir=None, submit=False,
-                            batch=True, debug=False):
+def hngprep(subjects:list=None, config_file:str=None, normalization_method:str=None,
+            target_dir:str=None, target_suffix=None,
+            output_dir:str=None, output_suffix=None, log_dir=None, submit=False,
+            batch=True, debug=False):
     """The controller for intensity normalization - handles configuration, input scrubbing, file I/O and method selection.
 
     Args:
         subjects (list, optional): A list of subjects to process.
         config_file (str, optional): A path to a clpipe configuration file to override the project default.
-        rescaling_method (str, optional): The method of normalization to perform. Defaults to RESCALING_DEFAULT.
+        normalization_method (str, optional): The method of normalization to perform. Defaults to RESCALING_DEFAULT.
         target_dir (str, optional): A path to the directory of images to normalize. Defaults to DEFAULT_TARGET_DIR.
         target_suffix (str, optional): [description]. Defaults to None.
         output_dir (str): A path to the output directory of normalized images.
@@ -88,14 +86,15 @@ def intensity_normalization(subjects:list=None, config_file:str=None, method:str
     # If provided, override project config with parameter config
     config.config_updater(config_file)
     # For those provided, replace intensity normalization config values with parameter values
-    config.setup_intensity_normalization(target_dir, target_suffix, output_dir, output_suffix, method, log_dir)
-    
-    target_dir = Path(config.config["IntensityNormalizationOptions"]["TargetDirectory"])
-    target_suffix = config.config["IntensityNormalizationOptions"]["TargetSuffix"]
-    method = config.config["IntensityNormalizationOptions"]["Method"]
+    # TODO: Implement input params
+    # config.setup_hngprep(target_dir, target_suffix, output_dir, output_suffix, method, log_dir)
+    hngprep_config = config.config["HNGPrepOptions"]
 
+    target_dir = Path(hngprep_config["TargetDirectory"])
+    normalization_method = config["IntensityNormalizationMethod"]
+ 
     # Validate the provided rescaling method
-    if method not in METHODS: 
+    if normalization_method not in NORMALIZATION_METHODS: 
         raise ValueError(f"Invalid rescaling method: {method}")
 
     if subjects is None:
@@ -104,7 +103,7 @@ def intensity_normalization(subjects:list=None, config_file:str=None, method:str
 
     # TODO: Process these as batch jobs
     for subject in subjects:
-        normalize_subject(config, f'sub-{subject}')
+        hngprep_subject(hngprep_config, f'sub-{subject}')
 
 def calculate_10000_global_median(in_path: os.PathLike, out_path:os.PathLike,
         mask_path: os.PathLike=None, base_dir: os.PathLike=None, crashdump_dir: os.PathLike=None):
@@ -132,7 +131,8 @@ def calculate_10000_global_median(in_path: os.PathLike, out_path:os.PathLike,
 
     workflow.connect(mul_10000_node, "out_file", div_median_node, "in_file")
     workflow.connect(median_node, "out_stat", div_median_node, "operand_value")
-    workflow.run()
+    
+    return workflow
 
 def calculate_100_voxel_mean(in_path: os.PathLike, out_path: os.PathLike, base_dir: os.PathLike=None,
     crashdump_dir: os.PathLike=None):
@@ -154,9 +154,10 @@ def calculate_100_voxel_mean(in_path: os.PathLike, out_path: os.PathLike, base_d
 
     workflow.connect(mul100_node, "out_file", div_mean_node, "in_file")
     workflow.connect(mean_node, "out_file",  div_mean_node, "operand_file")
-    workflow.run()
+    
+    return workflow
 
-def _get_method(method_str: str) -> Callable:
+def _get_normalization_method(method_str: str) -> Callable:
     if method_str == RESCALING_10000_GLOBALMEDIAN:
         return calculate_10000_global_median
     elif method_str == RESCALING_100_VOXELMEAN:
@@ -171,34 +172,31 @@ def append_suffix(base_path: Path, suffix: str) -> Path:
 
     return f"{filename}_{suffix}"
 
-def normalize_subject(config: ClpipeConfigParser, subject: str):
+def hngprep_subject(config: ClpipeConfigParser, subject: str):
 
-    config = config.config['IntensityNormalizationOptions']
+    config = config.config['HNGPrepOptions']
     target_dir = Path(config['TargetDirectory'])
     output_dir = Path(config['OutputDirectory'])
     target_suffix = config['TargetSuffix']
     output_suffix = config['OutputSuffix']
-    method_str = config['Method']
+    normalization_method_str = config['Method']
     log_dir = Path(config['LogDirectory'])
 
-    method: Callable = _get_method(method_str)
+    normalization_method: Callable = _get_normalization_method(normalization_method_str)
 
-    sub_output_dir = output_dir / method_str / subject
+    sub_output_dir = output_dir / subject
 
     if not sub_output_dir.exists():
         LOG.info(f"Creating directory: f{str(sub_output_dir)}")
         sub_output_dir.mkdir(parents=True, exist_ok=True)
 
     for image_path in target_dir.glob(f'{subject}/**/*{target_suffix}*'):
-        LOG.info(f"Normalization target: {target_suffix}")
-        LOG.info(f"Normalization method: {method.__name__}")
+        LOG.info(f"HNGPrep target: {target_suffix}")
+        LOG.info(f"Normalization method: {normalization_method.__name__}")
         
         output_path = sub_output_dir / append_suffix(image_path, output_suffix)
 
-        # for suffix in image_path.suffixes:
-        #     output_path = output_path.with_suffix(suffix)
-
-        method(image_path, output_path, base_dir=log_dir, crashdump_dir=log_dir)
+        normalization_workflow = normalization_method(image_path, output_path, base_dir=log_dir, crashdump_dir=log_dir)
 
         LOG.info(f"Rescaling complete and saved to: {output_path}")
 
