ceb7c956378542fa836fb91c6a9d761e66a82bf0
Will Asciutto <wasciutto@unc.edu>
2021-10-06 19:32:27 -0400

    Adjust the out_file path for butterworth node.

--- clpipe/fmri_postprocess2.py
+++ clpipe/fmri_postprocess2.py
@@ -8,21 +8,26 @@ class PostProcessSubjectJob():
     wf: pe.Workflow = None
     config: dict = None
     
-    def __init__(self, config, subject_id: str, in_file: os.PathLike, out_file: os.PathLike):
+    def __init__(self, config, subject_id: str, in_file: os.PathLike, out_file: os.PathLike, crashdump_dir: str=None):
         #self.ppconfig = config["PostProcessingOptions"]
         self.config=config
         self.subject_id=subject_id
         self.in_file=in_file
         self.out_file=out_file
         self.wf = pe.Workflow(name=PostProcessSubjectJob.__class__.__name__ + subject_id)
+        if crashdump_dir is not None:
+            self.wf.config['execution']['crashdump_dir'] = crashdump_dir
         self._compose_workflow()
 
     def _compose_workflow(self):
         voxel_mean_wf = build_100_voxel_mean_workflow(None, self.out_file, base_dir=self.wf.base_dir)
         butterworth_node = pe.Node(ButterworthFilter(in_file=self.in_file,
                                 hp=.008,lp=-1,order=2,tr=2), name="butterworth_filter")
-
-        self.wf.add_nodes([butterworth_node, voxel_mean_wf])
+    
+        self.wf.connect([
+            (butterworth_node, voxel_mean_wf, [("out_file","mean.in_file"),
+                                               ("out_file","mul100.in_file")])
+        ])
 
     def run(self):
         self.wf.run()
--- clpipe/postprocutils/nodes.py
+++ clpipe/postprocutils/nodes.py
@@ -4,13 +4,13 @@ import os
 
 from nipype.interfaces.base import BaseInterface, \
     BaseInterfaceInputSpec, traits, File, TraitedSpec
+from nipype.utils.filemanip import split_filename
 
 from clpipe.postprocutils.utils import apply_filter, calc_filter
 
 
 class ButterworthFilterInputSpec(BaseInterfaceInputSpec):
-    in_file = File(exists=True, desc='Image to be normalized', mandatory=True)
-    out_file = File(exists=False, desc='Output image path', mandatory=True)
+    in_file = File(exists=True, desc='Image to be normalized', mandatory=False)
     hp = traits.Float(desc='High-pass cutoff value. Set to -1 to disable',
                              mandatory=True)
     lp = traits.Float(desc='Low-pass cutoff value. Set to -1 to disable',
@@ -20,7 +20,7 @@ class ButterworthFilterInputSpec(BaseInterfaceInputSpec):
 
 
 class ButterworthFilterOutputSpec(TraitedSpec):
-    out_file = File(exists=True, desc="Filtered image", mandatory=True)
+    out_file = File(exists=True, desc="Filtered image")
 
 class ButterworthFilter(BaseInterface):
     input_spec = ButterworthFilterInputSpec
@@ -35,11 +35,13 @@ class ButterworthFilter(BaseInterface):
         filtered_data = apply_filter(filter, data)
 
         new_img = nb.Nifti1Image(filtered_data, img.affine, img.header)
-        nb.save(new_img, self.inputs.out_file)
+        _, base, _ = split_filename(fname)
+        self.new_file = base + '_filtered.nii'
+        nb.save(new_img, self.new_file)
 
         return runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['out_file'] = os.path.abspath(self.inputs.out_file)
+        outputs['out_file'] = os.path.abspath(self.new_file)
         return outputs
