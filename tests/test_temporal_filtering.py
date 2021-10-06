5320f530b5e476f100ebd35325ea88d5841b6339
Will Asciutto <wasciutto@unc.edu>
2021-10-06 16:34:35 -0400

    Fill out test for temporal filter.

--- tests/test_temporal_filtering.py
+++ tests/test_temporal_filtering.py
@@ -1,22 +1,29 @@
 import sys
 import logging
 import pytest
+import os
 
 import numpy as np
 import nibabel as nib
 
 sys.path.append('../clpipe')
-from clpipe.hngprep import temporal_filter
+from clpipe.postprocutils.nodes import ButterworthFilter
+import nipype.pipeline.engine as pe
 
 logging.basicConfig(level=logging.INFO)
 
 
-def test_temporal_filter(tmp_path, random_nii):
+def test_temporal_filter(tmp_path, sample_raw_image, workflow_base):
     filtered_path = tmp_path / "filtered.nii"
-    temporal_workflow = temporal_filter(random_nii, filtered_path)
-    temporal_workflow.run()
     
-    assert True
+    butterworth_node = pe.Node(ButterworthFilter(in_file=sample_raw_image,
+                                hp=.008,lp=-1,order=2,tr=2,out_file=filtered_path), name="Butterworth_Filter")
+    workflow_base.add_nodes([butterworth_node])
+    workflow_base.run()
+    workflow_base.write_graph(dotfilename = tmp_path / "filteredflow", graph2use='flat')
+
+    newImage = nib.load(filtered_path)
+    print(newImage)
 
 @pytest.mark.skip(reason="Not implemented")
 def test_temporal_filter_low_pass(tmp_path):
