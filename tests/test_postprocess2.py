83305e2f9ff9c7b44f3d8ddbca36a59113b23bc1
Will Asciutto <wasciutto@unc.edu>
2021-10-18 12:06:03 -0400

    Add command to run wf in test.

--- tests/test_postprocess2.py
+++ tests/test_postprocess2.py
@@ -3,7 +3,7 @@ from clpipe.fmri_postprocess2 import PostProcessSubjectJob
 def test_hngprep_subject(clpipe_config_default, tmp_path, sample_raw_image):
     postProcessSubjectJob = PostProcessSubjectJob(clpipe_config_default, 'sub-0', sample_raw_image, tmp_path / "postProcessed.nii.gz")
     postProcessSubjectJob.wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='flat')
-
+    postProcessSubjectJob.run()
     assert True
 
     # expected_path = Path(hngprep_config.config['HNGPrepOptions']['OutputDirectory']) \
