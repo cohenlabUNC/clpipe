e3f7f2e1a0672d385f808fd6b278673f1867b915
Will Asciutto <wasciutto@unc.edu>
2021-11-04 14:20:26 -0400

    Add susan smoothing workflow builder.

--- clpipe/postprocutils/workflows.py
+++ clpipe/postprocutils/workflows.py
@@ -1,7 +1,11 @@
 import os
 
+from math import sqrt, log
+
 from nipype.interfaces.fsl.maths import MeanImage, BinaryMaths, MedianImage
 from nipype.interfaces.fsl.utils import ImageStats
+from nipype.interfaces.fsl import SUSAN
+from nipype.interfaces.utility import Function
 import nipype.pipeline.engine as pe
 
 from .nodes import ButterworthFilter
@@ -97,36 +101,56 @@ def build_100_voxel_mean_workflow(in_file: os.PathLike=None, out_file: os.PathLi
 
     return workflow
 
-def add_100_voxel_mean_wf(base_workflow, base_dir: os.PathLike=None,
-    crashdump_dir: os.PathLike=None):
-    """Perform intensity normalization using the 100 voxel mean method.
-
-    Args:
-        in_path (str): A path to an input .nii to normalize.
-        out_path (str): A path to save the normalized image.
-    """
+def build_spatial_smoothing_workflow(in_file: os.PathLike=None, mask_path: os.PathLike=None, fwhm_mm: int=6, out_file: os.PathLike=None, 
+    base_dir: os.PathLike=None, crashdump_dir: os.PathLike=None):
     
-    if in_file != None:
-        mean_image = MeanImage(in_file=in_file)
-        mul_math = BinaryMaths(operation='mul', operand_value=100, in_file=in_file)
-    else:
-        mean_image = MeanImage()
-        mul_math = BinaryMaths(operation='mul', operand_value=100)
-    
-    mean_node = pe.Node(mean_image, name='mean')
-    mul100_node = pe.Node(mul_math, name="mul100")
-
-    if out_file != None:
-        div_math = BinaryMaths(operation='div', out_file=out_file)
-    else:
-        div_math = BinaryMaths(operation='div')
-    div_mean_node = pe.Node(div_math, name="div_mean") #operand_file=mean_path
-
-    workflow = pe.Workflow(name=RESCALING_100_VOXELMEAN, base_dir=base_dir)
+    workflow = pe.Workflow(name="spatial_smoothing", base_dir=base_dir)
     if crashdump_dir is not None:
         workflow.config['execution']['crashdump_dir'] = crashdump_dir
+    
+    fwhm_to_sigma = sqrt(8 * log(2))
+    sigma = fwhm_mm / fwhm_to_sigma
+    print(f"fwhm_to_sigma: {fwhm_to_sigma}")
+    
+    if mask_path is None:
+        print(f"No mask detected")
+        p2_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-p 2"), name='p2')
+        median_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-p 50"), name='median')
+    else:
+        print(f"Mask detected")
+        p2_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-k %s -p 2", mask_file=mask_path), name='p2')
+        median_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-k %s -p 50", mask_file=mask_path), name='median')
+    
+    # Use an arbitrary function node to calculate the susan threshold from two scalars with helper function
+    susan_thresh_node = pe.Node(Function(inputs_names=["median_intensity", "p2_intensity"], output_names=["susan_threshold"], function=_calc_susan_threshold), name="susan_threshold")
+    
+    mean_image_node = pe.Node(MeanImage(in_file=in_file), name="mean_image")
 
-    workflow.connect(mul100_node, "out_file", div_mean_node, "in_file")
-    workflow.connect(mean_node, "out_file",  div_mean_node, "operand_file")
+    # Setup calculations for susan threshold
+    workflow.connect(median_intensity_node, "out_stat", susan_thresh_node, "median_intensity")
+    workflow.connect(p2_intensity_node, "out_stat", susan_thresh_node, "p2_intensity")
 
+    # Setup susan command
+    #   Usage: susan <input> <bt> <dt> <dim> <use_median> <n_usans> [<usan1> <bt1> [<usan2> <bt2>]] <output>
+    susan_node = pe.Node(SUSAN(in_file=in_file, fwhm=sigma, use_median=1, dimension=3, out_file=out_file), name="SUSAN")
+    
+    workflow.connect(susan_thresh_node, "susan_threshold", susan_node, "brightness_threshold")
+    #workflow.connect(mean_image_node, "out_file", susan_node, "")
+    
+    if mask_path is not None:
+        # run_fsl_command(glue("fslmaths {out_file} -mas {brain_mask} {out_file} -odt float"), log_file = log_file)
+        pass
+        
     return workflow
+    
+def _calc_susan_threshold(median_intensity, p2_intensity):
+    return (median_intensity - p2_intensity) * .75
+ 
+    
+    
+    
+    
+    
+    
+    
+    pass
--- tests/test_postprocess2.py
+++ tests/test_postprocess2.py
@@ -1,14 +1,47 @@
-from clpipe.fmri_postprocess2 import PostProcessSubjectJob
+import pytest
 
-def test_hngprep_subject(clpipe_config_default, tmp_path, sample_raw_image):
-    postProcessSubjectJob = PostProcessSubjectJob(clpipe_config_default, 'sub-0', sample_raw_image, tmp_path / "postProcessed.nii.gz")
+import nipype.pipeline.engine as pe
+import nibabel as nib
+
+from clpipe.fmri_postprocess2 import PostProcessSubjectJob
+from clpipe.postprocutils.workflows import build_10000_global_median_workflow, build_100_voxel_mean_workflow, build_spatial_smoothing_workflow
+from clpipe.postprocutils.nodes import ButterworthFilter
+
+@pytest.mark.skip(reason="Needs refactor")
+def test_postprocess_subject(clpipe_config_default, tmp_path, sample_raw_image):
+    postProcessSubjectJob = PostProcessSubjectJob(sample_raw_image, tmp_path / "postProcessed.nii.gz", tmp_path)
     postProcessSubjectJob.wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='flat')
     postProcessSubjectJob.run()
     assert True
 
-    # expected_path = Path(hngprep_config.config['HNGPrepOptions']['OutputDirectory']) \
-    #     / "TBD" \
-    #     / "sub-0" \
-    #     / "sub-0_task-rest_run-1_space-MNI152NLin2009cAsym_desc-hngprep_bold.nii.gz"
 
-    #assert expected_path.exists(), f"Expected path {expected_path} not found."
+def test_spatial_smoothing_wf(tmp_path, random_nii, random_nii_mask):
+    out_path = tmp_path / "smoothed.nii.gz"
+    wf = build_spatial_smoothing_workflow(in_file=random_nii, out_file=out_path, fwhm_mm=6, 
+        mask_path=random_nii_mask, base_dir=tmp_path, crashdump_dir=tmp_path)
+    wf.run()
+    wf.write_graph(dotfilename = tmp_path / "spatialSmoothFlow", graph2use='flat')
+    
+    assert True
+
+def test_calculate_100_voxel_mean_wf(tmp_path, sample_raw_image):
+    out_path = tmp_path / "normalized.nii.gz"
+    wf = build_100_voxel_mean_workflow(in_file=sample_raw_image, out_file=out_path, base_dir=tmp_path, crashdump_dir=tmp_path)
+    wf.run()
+    wf.write_graph(dotfilename = tmp_path / "calc100voxelMeanFlow", graph2use='flat')
+
+    assert True
+    
+def test_butterworth_filter(tmp_path, sample_raw_image, workflow_base):
+    filtered_path = tmp_path / "Test_Workflow" / "Butterworth_Filter" / "sample_raw_filtered.nii"
+    
+    butterworth_node = pe.Node(ButterworthFilter(in_file=sample_raw_image,
+                                hp=.008,lp=-1,order=2,tr=2), name="Butterworth_Filter")
+    workflow_base.add_nodes([butterworth_node])
+    workflow_base.run()
+    workflow_base.write_graph(dotfilename = tmp_path / "filteredflow", graph2use='flat')
+
+    newImage = nib.load(filtered_path)
+    print(newImage)
+    
+    assert True
