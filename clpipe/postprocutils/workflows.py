793d76f055491837acc994d38552da276e7a94f2
Will <wasciutto@unc.edu>
2021-11-16 22:01:01 +0000

    Create hardcoded postproc flow. Adjust in/out/mask file settings for sub workflows.

--- clpipe/postprocutils/workflows.py
+++ clpipe/postprocutils/workflows.py
@@ -15,7 +15,7 @@ RESCALING_100_VOXELMEAN = "100_voxelmean"
 NORMALIZATION_METHODS = (RESCALING_10000_GLOBALMEDIAN, RESCALING_100_VOXELMEAN)
 
 def build_postprocessing_workflow(name, in_path: os.PathLike, out_path:os.PathLike, 
-    temporal_filter=True, intensity_normalize=True, base_dir: os.PathLike=None, 
+    mask_path: os.PathLike=None, temporal_filter=True, intensity_normalize=True, base_dir: os.PathLike=None, 
     crashdump_dir: os.PathLike=None):
     
     wf = pe.Workflow(name=name, base_dir=base_dir)
@@ -26,21 +26,28 @@ def build_postprocessing_workflow(name, in_path: os.PathLike, out_path:os.PathLi
     # The most recently added node or wf
     previous_step = None
 
-    if temporal_filter:
-        butterworth_node = pe.Node(ButterworthFilter(in_file=in_path,
-            hp=.008,lp=-1,order=2,tr=2), name="butterworth_filter")
-        previous_step = butterworth_node
-    if intensity_normalize:
-        voxel_mean_wf = build_10000_global_median_workflow(None, out_path, base_dir=wf.base_dir,
-            crashdump_dir=wf.config['execution']['crashdump_dir'])
-        wf.connect([
-            (previous_step, voxel_mean_wf, [("out_file","global_median.in_file"),
-                                            ("out_file","mul_10000.in_file")])
-        ])
+    # Initialize WF Components
+    butterworth_node = pe.Node(ButterworthFilter(hp=.008,lp=-1,order=2,tr=2), name="butterworth_filter")
+    normalize_10000_gm_wf = build_10000_global_median_workflow(base_dir=wf.base_dir, mask_path=mask_path, crashdump_dir=wf.config['execution']['crashdump_dir'])
+    smoothing_wf = build_spatial_smoothing_workflow(base_dir=wf.base_dir, mask_path=mask_path, crashdump_dir=wf.config['execution']['crashdump_dir'])
+
+    butterworth_node.inputs.in_file = in_path
+
+    wf.connect([
+            (butterworth_node, normalize_10000_gm_wf, [("out_file","global_median.in_file"),
+                                                       ("out_file","mul_10000.in_file")])
+    ])
+    wf.connect([
+            (normalize_10000_gm_wf, smoothing_wf, [("div_median.out_file","p2.in_file"),
+                                                   ("div_median.out_file","median.in_file"),
+                                                   ("div_median.out_file", "SUSAN.in_file")])
+    ])
+
+    smoothing_wf.inputs.SUSAN.out_file = out_path
     
     return wf
 
-def build_10000_global_median_workflow(in_path: os.PathLike, out_path:os.PathLike,
+def build_10000_global_median_workflow(in_path: os.PathLike=None, out_path:os.PathLike=None,
         mask_path: os.PathLike=None, base_dir: os.PathLike=None, crashdump_dir: os.PathLike=None):
     """Perform intensity normalization using the 10,000 global median method.
 
@@ -51,19 +58,20 @@ def build_10000_global_median_workflow(in_path: os.PathLike, out_path:os.PathLik
         base_dir (os.PathLike, optional): A path to the base directory for the workflow.
     """
 
-    median_node = pe.Node(ImageStats(op_string="-k %s -p 50"), name='global_median')
+    median_node = pe.Node(ImageStats(op_string="-p 50"), name='global_median')
     mul_10000_node = pe.Node(BinaryMaths(operation="mul", operand_value=10000), name="mul_10000")
     div_median_node = pe.Node(BinaryMaths(operation="div"), name="div_median")
 
-    if in_path != None:
+    if in_path:
         median_node.inputs.in_file = in_path
         mul_10000_node.inputs.in_file = in_path
 
-    if out_path != None:
+    if out_path:
         div_median_node.inputs.out_file = out_path
 
-    if mask_path != None:
+    if mask_path:
         median_node.inputs.mask_file = mask_path
+        median_node.inputs.op_string = "-k %s -p 50"
 
 
     workflow = pe.Workflow(name=RESCALING_10000_GLOBALMEDIAN, base_dir=base_dir)
@@ -110,7 +118,6 @@ def build_100_voxel_mean_workflow(in_file: os.PathLike=None, out_file: os.PathLi
 
     return workflow
 
-#TODO: Rewrite to not use multiple instantiation variants
 def build_spatial_smoothing_workflow(in_file: os.PathLike=None, mask_path: os.PathLike=None, fwhm_mm: int=6, out_file: os.PathLike=None, 
     base_dir: os.PathLike=None, crashdump_dir: os.PathLike=None):
     
@@ -122,19 +129,20 @@ def build_spatial_smoothing_workflow(in_file: os.PathLike=None, mask_path: os.Pa
     sigma = fwhm_mm / fwhm_to_sigma
     print(f"fwhm_to_sigma: {fwhm_to_sigma}")
     
-    if mask_path is None:
-        print(f"No mask detected")
-        p2_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-p 2"), name='p2')
-        median_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-p 50"), name='median')
-    else:
-        print(f"Mask detected")
-        p2_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-k %s -p 2", mask_file=mask_path), name='p2')
-        median_intensity_node = pe.Node(ImageStats(in_file=in_file, op_string="-k %s -p 50", mask_file=mask_path), name='median')
+    # Setup nodes to calculate susan threshold inputs
+    p2_intensity_node = pe.Node(ImageStats(op_string="-p 2"), name='p2')
+    median_intensity_node = pe.Node(ImageStats(op_string="-p 50"), name='median')
+    
+    # Add mask to commands
+    if mask_path:
+        print(f"Using mask: {mask_path}")
+        p2_intensity_node.inputs.mask_file = mask_path
+        median_intensity_node.inputs.mask_file = mask_path
+        p2_intensity_node.inputs.op_string = "-k %s -p 2"
+        median_intensity_node.inputs.op_string = "-k %s -p 50"
     
     # Use an arbitrary function node to calculate the susan threshold from two scalars with helper function
     susan_thresh_node = pe.Node(Function(inputs_names=["median_intensity", "p2_intensity"], output_names=["susan_threshold"], function=_calc_susan_threshold), name="susan_threshold")
-    
-    mean_image_node = pe.Node(MeanImage(in_file=in_file), name="mean_image")
 
     # Setup calculations for susan threshold
     workflow.connect(median_intensity_node, "out_stat", susan_thresh_node, "median_intensity")
@@ -142,8 +150,19 @@ def build_spatial_smoothing_workflow(in_file: os.PathLike=None, mask_path: os.Pa
 
     # Setup susan command
     #   Usage: susan <input> <bt> <dt> <dim> <use_median> <n_usans> [<usan1> <bt1> [<usan2> <bt2>]] <output>
-    susan_node = pe.Node(SUSAN(in_file=in_file, fwhm=sigma, use_median=1, dimension=3, out_file=out_file), name="SUSAN")
+    susan_node = pe.Node(SUSAN(fwhm=sigma, use_median=1, dimension=3), name="SUSAN")
     
+    #nmean_image_node = pe.Node(MeanImage(), name="mean_image")
+
+    # Set WF inputs and outputs
+    if in_file:
+        p2_intensity_node.inputs.in_file = in_file
+        median_intensity_node.inputs.in_file = in_file
+        susan_node.inputs.in_file = in_file
+        #mean_image_node.inputs.in_file = in_file
+    if out_file:
+        susan_node.inputs.out_file = out_file
+
     workflow.connect(susan_thresh_node, "susan_threshold", susan_node, "brightness_threshold")
     #workflow.connect(mean_image_node, "out_file", susan_node, "")
     
