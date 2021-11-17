28daf5a6da6bc94fb99f76073473fb68795dcec0
Will <wasciutto@unc.edu>
2021-11-17 21:20:19 +0000

    Add Helper class and associated fixtures. Rearrange fixture order based on relevance.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,13 @@ import pytest
 import numpy as np
 import sys
 import os
+from pathlib import Path
+
 import nibabel as nib
 import nipype.pipeline.engine as pe
-from pathlib import Path
+from nilearn import plotting
+from nilearn.image import load_img, index_img
+
 from click.testing import CliRunner
 
 sys.path.append('../clpipe')
@@ -15,14 +19,67 @@ PROJECT_TITLE = "test_project"
 NUM_SUBJECTS = 8
 DEFAULT_RANDOM_NII_DIMS = (12, 12, 12, 36)
 
+class Helpers:
+    @staticmethod
+    def plot_4D_img_slice(image_path: Path, png_name: str):
+        image = load_img(str(image_path))
+        image_slice = index_img(image, 1)
+    
+        plot_path = image_path.parent / png_name
+        plotting.plot_img(image_slice, output_file=plot_path)
+
+    @staticmethod
+    def create_test_dir(artifact_dir: Path, name: str):
+        test_path = artifact_dir / name
+        test_path.mkdir(parents=True, exist_ok=True)
+
+        return test_path
+
 def pytest_addoption(parser):
     parser.addoption(
         "--plot_img", action="store_true", default=False, help="Save plot of image processing results as .png"
     )
+    parser.addoption(
+        "--write_graph", action="store", default=False, help="Save plot of image processing results as .png"
+    )
+
+@pytest.fixture
+def helpers():
+    return Helpers
 
 @pytest.fixture
 def plot_img(request):
     return request.config.getoption("--plot_img")
+
+@pytest.fixture
+def write_graph(request):
+    return request.config.getoption("--write_graph")
+
+@pytest.fixture
+def artifact_dir():
+    return Path("tests", "artifacts").resolve()
+
+@pytest.fixture(scope="package")
+def sample_raw_image() -> Path:
+    """
+    This is image is from the OpenNeuro dataset:
+    'Interoception during aging: The heartbeat detection task'
+    Located at https://openneuro.org/datasets/ds003763/versions/1.0.0
+
+    The image consists of slices 100-110 of sub-09113/func/sub-09113_task-heart_bold.nii.gz
+    """
+    
+    return Path("tests/data/sample_raw.nii.gz").resolve()
+
+@pytest.fixture(scope="package")
+def sample_raw_image_mask() -> Path:
+    """
+    This image is a mask of the sample_raw_image fixture.
+
+    The mask was calculated with nilearn's masking.compute_epi_mask function.
+    """
+    
+    return Path("tests/data/sample_raw_mask.nii.gz").resolve()
 
 @pytest.fixture(scope="module")
 def clpipe_dir(tmp_path_factory):
@@ -83,6 +140,12 @@ def clpipe_fmriprep_dir(clpipe_dir):
 def clpipe_config_default():
     return ClpipeConfigParser()
 
+@pytest.fixture(scope="function")
+def workflow_base(tmp_path):
+    wf =  pe.Workflow(name="Test_Workflow", base_dir=tmp_path)
+    wf.config['execution']['crashdump_dir'] = "nypipe_crashdumps"
+    return wf
+
 def generate_random_nii(dims: tuple=DEFAULT_RANDOM_NII_DIMS, low: int=0, high: int=1000) -> nib.Nifti1Image:
     """Creates a simple nii image with the given dimensions.
 
@@ -104,9 +167,6 @@ def generate_random_nii(dims: tuple=DEFAULT_RANDOM_NII_DIMS, low: int=0, high: i
     image = nib.Nifti1Image(array_data, affine)
 
     return image
-
-# TODO: generalize the nii create based on arbitrary array input
-#def generate_nii(data: np.array) -> nib.Nifti1Image:
 
 def generate_random_nii_mask(dims: tuple=DEFAULT_RANDOM_NII_DIMS) -> nib.Nifti1Image:
     mask_base = np.ones(dims, dtype=np.int16)
@@ -145,32 +205,3 @@ def random_nii_mask(tmp_path) -> Path:
 
     nib.save(nii, nii_path)
     return nii_path
-
-
-@pytest.fixture(scope="function")
-def workflow_base(tmp_path):
-    wf =  pe.Workflow(name="Test_Workflow", base_dir=tmp_path)
-    wf.config['execution']['crashdump_dir'] = "nypipe_crashdumps"
-    return wf
-
-@pytest.fixture(scope="package")
-def sample_raw_image() -> Path:
-    """
-    This is image is from the OpenNeuro dataset:
-    'Interoception during aging: The heartbeat detection task'
-    Located at https://openneuro.org/datasets/ds003763/versions/1.0.0
-
-    The image consists of slices 100-110 of sub-09113/func/sub-09113_task-heart_bold.nii.gz
-    """
-    
-    return Path("tests/data/sample_raw.nii.gz").resolve()
-
-@pytest.fixture(scope="package")
-def sample_raw_image_mask() -> Path:
-    """
-    This image is a mask of the sample_raw_image fixture.
-
-    The mask was calculated with nilearn's masking.compute_epi_mask function.
-    """
-    
-    return Path("tests/data/sample_raw_mask.nii.gz").resolve()
