36946496af05343692ad14618a8ab7344f1bc683
Will <wasciutto@unc.edu>
2021-11-17 21:21:17 +0000

    Alter tests to use persistent test dirs and use switches for writing graphs/images.

--- tests/test_postprocess2_ordering.py
+++ tests/test_postprocess2_ordering.py
@@ -7,77 +7,104 @@ from nilearn.image import load_img, index_img
 
 from clpipe.postprocutils.workflows import *
 
-def test_postprocess_wf(clpipe_config_default, tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "postProcessed.nii.gz"
+def test_postprocess_wf_order_1(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postProcessed.nii.gz"
     
     wf = build_postprocessing_workflow("postproc_test", sample_raw_image, out_path, 
-        processing_steps=["temporal_filtering", "intensity_normalization", "spatial_smoothing"], base_dir=tmp_path, crashdump_dir=tmp_path)
-    wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='colored')
+        processing_steps=["temporal_filtering", "intensity_normalization", "spatial_smoothing"], base_dir=test_path, crashdump_dir=test_path)
+    
     wf.run()
 
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "postProcessSubjectFlow", graph2use=write_graph)
+   
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "postProcessed.png"))
+        helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
 
-def test_postprocess_wf_order2(clpipe_config_default, tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "postProcessed.nii.gz"
+def test_postprocess_wf_order_2(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postProcessed.nii.gz"
     
     wf = build_postprocessing_workflow("postproc_test", sample_raw_image, out_path, 
-        processing_steps=["intensity_normalization", "temporal_filtering", "spatial_smoothing"], base_dir=tmp_path, crashdump_dir=tmp_path)
-    wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='colored')
+        processing_steps=["intensity_normalization", "temporal_filtering", "spatial_smoothing"], base_dir=test_path, crashdump_dir=test_path)
+    
     wf.run()
 
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "postProcessSubjectFlow", graph2use=write_graph)
+   
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "postProcessed.png"))
+        helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
 
-def test_postprocess_wf_order3(clpipe_config_default, tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "postProcessed.nii.gz"
+def test_postprocess_wf_order_3(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postProcessed.nii.gz"
     
     wf = build_postprocessing_workflow("postproc_test", sample_raw_image, out_path, 
-        processing_steps=["spatial_smoothing", "temporal_filtering", "intensity_normalization"], base_dir=tmp_path, crashdump_dir=tmp_path)
-    wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='colored')
+        processing_steps=["spatial_smoothing", "temporal_filtering", "intensity_normalization"], base_dir=test_path, crashdump_dir=test_path)
+    
     wf.run()
 
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "postProcessSubjectFlow", graph2use=write_graph)
+   
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "postProcessed.png"))
+        helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
 
-def test_postprocess_wf_order4(clpipe_config_default, tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "postProcessed.nii.gz"
+def test_postprocess_wf_order_4(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postProcessed.nii.gz"
     
     wf = build_postprocessing_workflow("postproc_test", sample_raw_image, out_path, 
-        processing_steps=["temporal_filtering", "intensity_normalization"], base_dir=tmp_path, crashdump_dir=tmp_path)
-    wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='colored')
+        processing_steps=["temporal_filtering", "intensity_normalization"], base_dir=test_path, crashdump_dir=test_path)
+    
     wf.run()
 
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "postProcessSubjectFlow", graph2use=write_graph)
+   
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "postProcessed.png"))
+        helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
 
-def test_postprocess_wf_order5(clpipe_config_default, tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "postProcessed.nii.gz"
+def test_postprocess_wf_order_5(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postProcessed.nii.gz"
     
     wf = build_postprocessing_workflow("postproc_test", sample_raw_image, out_path, 
-        processing_steps=["intensity_normalization", "temporal_filtering"], base_dir=tmp_path, crashdump_dir=tmp_path)
-    wf.write_graph(dotfilename = tmp_path / "postProcessSubjectFlow", graph2use='colored')
+        processing_steps=["intensity_normalization", "spatial_smoothing"], base_dir=test_path, crashdump_dir=test_path)
+    
     wf.run()
 
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "postProcessSubjectFlow", graph2use=write_graph)
+   
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "postProcessed.png"))
+        helpers.plot_4D_img_slice(out_path, "postProcessed.png")
+
+    assert True
+
+def test_postprocess_wf_order_6(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postProcessed.nii.gz"
+    
+    wf = build_postprocessing_workflow("postproc_test", sample_raw_image, out_path, 
+        processing_steps=["temporal_filtering", "spatial_smoothing"], base_dir=test_path, crashdump_dir=test_path)
+    
+    wf.run()
+
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "postProcessSubjectFlow", graph2use=write_graph)
+   
+    if plot_img:
+        helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
--- tests/test_postprocess2_wfs.py
+++ tests/test_postprocess2_wfs.py
@@ -4,61 +4,71 @@ import nipype.pipeline.engine as pe
 import nibabel as nib
 from nilearn import plotting
 from nilearn.image import load_img, index_img
+import os
+from pathlib import Path
 
 from clpipe.postprocutils.workflows import *
 
-def test_spatial_smoothing_wf(tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "smoothed.nii.gz"
+def test_spatial_smoothing_wf(artifact_dir, request, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    
+    out_path = test_path / "smoothed.nii.gz"
     wf = build_spatial_smoothing_workflow(in_file=sample_raw_image, out_file=out_path, fwhm_mm=6, 
-        mask_path=sample_raw_image_mask, base_dir=tmp_path, crashdump_dir=tmp_path)
+        mask_path=sample_raw_image_mask, base_dir=test_path, crashdump_dir=test_path)
     wf.run()
-    wf.write_graph(dotfilename = tmp_path / "spatialSmoothFlow", graph2use='colored')
+
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "filteredflow", graph2use=write_graph)
 
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "smoothed.png"))
+        helpers.plot_4D_img_slice(out_path, "smoothed.png")
     
     assert True
 
-def test_calculate_100_voxel_mean_wf(tmp_path, sample_raw_image, plot_img):
-    out_path = tmp_path / "normalized_100vm.nii.gz"
-    wf = build_100_voxel_mean_workflow(in_file=sample_raw_image, out_file=out_path, base_dir=tmp_path, crashdump_dir=tmp_path)
+def test_calculate_100_voxel_mean_wf(artifact_dir, sample_raw_image, plot_img, write_graph, request, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+
+    out_path = test_path / "normalized_100vm.nii.gz"
+    wf = build_100_voxel_mean_workflow(in_file=sample_raw_image, out_file=out_path, base_dir=test_path, crashdump_dir=test_path)
     wf.run()
-    wf.write_graph(dotfilename = tmp_path / "calc100voxelMeanFlow", graph2use='colored')
+
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "calc100voxelMeanFlow", graph2use=write_graph)
 
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "normalized_100vm.png"))
+        helpers.plot_4D_img_slice(out_path, "normalized_100vm.png")
 
     assert True
 
-def test_calculate_10000_global_median_wf(tmp_path, sample_raw_image, sample_raw_image_mask, plot_img):
-    out_path = tmp_path / "normalized_10000gm.nii.gz"
+def test_calculate_10000_global_median_wf(artifact_dir, sample_raw_image, sample_raw_image_mask, plot_img, write_graph, request, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
     
-    wf = build_10000_global_median_workflow(in_file=sample_raw_image, out_file=out_path, mask_file=sample_raw_image_mask, base_dir=tmp_path, crashdump_dir=tmp_path)
+    out_path = test_path / "normalized_10000gm.nii.gz"
+    
+    wf = build_10000_global_median_workflow(in_file=sample_raw_image, out_file=out_path, mask_file=sample_raw_image_mask, base_dir=test_path, crashdump_dir=test_path)
     wf.run()
-    wf.write_graph(dotfilename = tmp_path / "calc10000globalMedianFlow", graph2use='colored')
+
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "calc10000globalMedianFlow", graph2use=write_graph)
 
     if plot_img:
-        image = load_img(str(out_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "normalized_10000gm.png"))
+        helpers.plot_4D_img_slice(out_path, "normalized_10000gm.png")
 
     assert True
     
-def test_butterworth_filter_wf(tmp_path, sample_raw_image, plot_img):
-    filtered_path = tmp_path / "sample_raw_filtered.nii"
+def test_butterworth_filter_wf(artifact_dir, sample_raw_image, plot_img, write_graph, request, helpers):
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    
+    filtered_path = test_path / "sample_raw_filtered.nii"
 
     wf = build_butterworth_filter_workflow(hp=.008, lp=-1, tr=2, order=2, in_file=sample_raw_image, out_file=filtered_path, 
-        base_dir=tmp_path, crashdump_dir=tmp_path)
+        base_dir=test_path, crashdump_dir=test_path)
     wf.run()
-    wf.write_graph(dotfilename = tmp_path / "filteredflow", graph2use='colored')
+
+    if write_graph:
+        wf.write_graph(dotfilename = test_path / "filteredflow", graph2use=write_graph)
 
     if plot_img:
-        image = load_img(str(filtered_path))
-        image_slice = index_img(image, 1)
-        plotting.plot_img(image_slice, output_file= str(tmp_path / "filtered.png"))
+        helpers.plot_4D_img_slice(filtered_path, "filtered.png")
     
     assert True
