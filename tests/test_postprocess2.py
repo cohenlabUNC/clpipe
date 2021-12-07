138674d174763b7775c458a596219f948068ea60
Will <wasciutto@unc.edu>
2021-12-07 22:37:27 +0000

    Update cli tests.

--- tests/test_postprocess2.py
+++ tests/test_postprocess2.py
@@ -13,7 +13,9 @@ from clpipe.postprocutils.workflows import *
 from clpipe.postprocutils.confounds import prepare_confounds
 from clpipe.fmri_postprocess2 import PostProcessSubjectJobs, PostProcessSubjectJob, postprocess_fmriprep_dir, fmri_postprocess2_cli
 
-def test_postprocess_cli_direct(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+def test_postprocess_cli_debug(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+    """Note: this test always passes because click does its own exit code handling - but this lets one trace through the cli with a debugger"""
+
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
     config = clpipe_fmriprep_dir / "clpipe_config.json"
     glm_config = clpipe_fmriprep_dir / "glm_config.json"
@@ -22,12 +24,35 @@ def test_postprocess_cli_direct(clpipe_fmriprep_dir, artifact_dir, helpers, requ
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    fmri_postprocess2_cli(['-config_file', str(config),
-                            '-target_dir', str(fmriprep_dir),
-                            '-output_dir', str(postproc_dir),
-                            '-glm_config_file', str(glm_config),
-                            '-log_dir', str(log_dir),
-                            '-no-batch', '-submit', '-debug'])
+    with pytest.raises(SystemExit):
+        fmri_postprocess2_cli(['-config_file', str(config),
+                                '-target_dir', str(fmriprep_dir),
+                                '-output_dir', str(postproc_dir),
+                                '-glm_config_file', str(glm_config),
+                                '-log_dir', str(log_dir),
+                                '-no-batch', '-debug'])
+
+def test_postprocess_cli(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+    fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
+    config = clpipe_fmriprep_dir / "clpipe_config.json"
+    glm_config = clpipe_fmriprep_dir / "glm_config.json"
+    test_dir = helpers.create_test_dir(artifact_dir, request.node.name)
+    postproc_dir = Path(test_dir / "data_postprocessed")
+    log_dir = Path(test_dir / "logs" / "postproc_logs")
+    log_dir.mkdir(parents=True, exist_ok=True)
+    
+    runner = CliRunner()
+    result = runner.invoke(
+        hngprep_cli, 
+        ['-config_file', str(config),
+        '-target_dir', str(fmriprep_dir),
+        '-output_dir', str(postproc_dir),
+        '-glm_config_file', str(glm_config),
+        '-log_dir', str(log_dir),
+        'dsfdsf', '-debug']
+    )
+    traceback.print_exc(result.exception)
+    assert result.stderr == 0
 
 def test_postprocess_fmriprep_dir(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
@@ -109,4 +134,5 @@ def test_postprocess_subject(clpipe_fmriprep_dir, artifact_dir, helpers, request
 
     subject = PostProcessSubjectJob('1', clpipe_fmriprep_dir, postproc_dir, glm_config, log_dir=log_dir)
     subject.run()
+
 
