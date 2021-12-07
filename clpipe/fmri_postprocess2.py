4b61adaca270025d80ae316d7785c7439727de48
Will <wasciutto@unc.edu>
2021-12-07 20:35:52 +0000

    Change log_dir to folder parameter instead of boolean.

--- clpipe/fmri_postprocess2.py
+++ clpipe/fmri_postprocess2.py
@@ -29,11 +29,12 @@ PYBIDS_DB_PATH = "TestFiles/fmriprep_dir"
 @click.option('-target_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False), help="""Which fmriprep directory to process. 
     If a configuration file is provided with a BIDS directory, this argument is not necessary. 
     Note, must point to the ``fmriprep`` directory, not its parent directory.""")
-@click.option('-output_dir', type=click.Path(dir_okay=True, file_okay=False), help = """Where to put the postprocessed data. 
+@click.option('-output_dir', type=click.Path(dir_okay=True, file_okay=False), default=None, required=True, help = """Where to put the postprocessed data. 
     If a configuration file is provided with a output directory, this argument is not necessary.""")
-@click.option('-batch', is_flag = True, default=True, help = 'Flag to create batch jobs without prompt.')
-@click.option('-submit', is_flag = True, default=True, help = 'Flag to submit commands to the HPC without prompt.')
-@click.option('-log_dir', is_flag = True, default=True, help = 'Path to the logging directory.')
+@click.option('-log_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False), default=None, required = False, help = 'Path to the logging directory.')
+@click.option('-batch/-no-batch', is_flag = True, default=True, help = 'Flag to create batch jobs without prompt.')
+@click.option('-submit', is_flag = True, default=False, help = 'Flag to submit commands to the HPC without prompt.')
+
 @click.option('-debug', is_flag = True, default=False, help = 'Print detailed processing information and traceback for errors.')
 def fmri_postprocess2_cli(subjects, config_file, glm_config_file, target_dir, output_dir, batch, submit, log_dir, debug):
     postprocess_fmriprep_dir(subjects=subjects, config_file=config_file, glm_config_file=glm_config_file, fmriprep_dir=target_dir, output_dir=output_dir, 
@@ -57,31 +58,39 @@ def postprocess_subject(subject_id, fmriprep_dir, output_dir, glm_config, log_di
 def postprocess_fmriprep_dir(subjects=None, config_file=None, glm_config_file=None, fmriprep_dir=None, output_dir=None, 
     batch=False, submit=False, log_dir=None, debug=False):
 
-    # Setup Logging
-    if debug: LOG.setLevel(logging.DEBUG)
-    LOG.debug(f"Starting postprocessing job targeting: {fmriprep_dir}")
-    
     # Handle configuration
     config = ClpipeConfigParser()
     config.config_updater(config_file)
 
+    # Make sure any string paths are converted to Path type
+    config_file = Path(config_file)
+    glm_config_file = Path(glm_config_file)
+    fmriprep_dir = Path(fmriprep_dir)
+    output_dir = Path(output_dir)
+    log_dir = Path(log_dir)
+
+    # Setup Logging
+    if debug: LOG.setLevel(logging.DEBUG)
+    LOG.debug(f"Starting postprocessing job targeting: {str(fmriprep_dir)}")
+    click.echo(f"Starting postprocessing job targeting: {str(fmriprep_dir)}")
+
     # Create jobs based on subjects given for processing
     # TODO: PYBIDS_DB_PATH should have config arg
-    jobs_to_run = PostProcessSubjectJobs(fmriprep_dir, output_dir, subjects, glm_config_file, log_dir, PYBIDS_DB_PATH)
+    jobs_to_run = PostProcessSubjectJobs(fmriprep_dir, output_dir, glm_config_file, subjects, log_dir, PYBIDS_DB_PATH)
 
     # Setup batch jobs if indicated
-    if batch or click.confirm('Would you to process these subjects on the HPC? (Choose "N" for local)'):
+    if batch:
         batch_manager = setup_batch_manager(config)
         jobs_to_run.set_batch_manager(batch_manager)
         click.echo(jobs_to_run.batch_manager.print_jobs())
 
-        if submit or click.confirm('Submit these jobs to the HPC?'):
+        if submit:
             jobs_to_run.run()
     # Otherwise, process the images locally
     else:
         click.echo(str(jobs_to_run))
 
-        if submit or click.confirm('Run these jobs locally?'):
+        if submit:
             jobs_to_run.run()
 
 def setup_batch_manager(config):
@@ -92,7 +101,7 @@ def setup_batch_manager(config):
 # TODO: throw exceptions here instead of exiting
 def get_subjects(fmriprep_dir: BIDSLayout, subjects):   
     # If no subjects were provided, use all subjects in the fmriprep directory
-    if subjects is None:
+    if subjects is None or len(subjects) == 0:
         try:
             subjects = fmriprep_dir.get_subjects()
             if len(subjects) == 0:
@@ -153,7 +162,7 @@ class PostProcessSubjectJob(CLPipeJob):
         return f"Postprocessing Job: {self.subject_id}"
 
     def run(self):
-        self.bids:BIDSLayout = BIDSLayout(self.fmriprep_dir, validate=False)
+        self.bids:BIDSLayout = BIDSLayout(self.fmriprep_dir, validate=False, index_metadata=False)
         
         # Find the subject's confounds file
         # TODO: Need switch here from config to determine if confounds wanted
@@ -211,7 +220,7 @@ class PostProcessSubjectJobs(CLPipeJob):
         # Get the fmriprep_dir as a BIDSLayout object
         if pybids_db_path and not os.path.exists(pybids_db_path):
             print("Indexing fMRIPrep directory...")
-        self.bids:BIDSLayout = BIDSLayout(fmriprep_dir, validate=False, database_path=self.pybids_db_path)
+        self.bids:BIDSLayout = BIDSLayout(fmriprep_dir, validate=False, database_path=self.pybids_db_path, index_metadata=False)
 
         # Choose the subjects to process
         self.subjects_to_process = get_subjects(self.bids, subjects_to_process)
