ba3a95c064901425fa615cdafabba5986b04de2b
Will <wasciutto@unc.edu>
2021-12-13 14:13:50 +0000

    Add class-based logging.

--- clpipe/fmri_postprocess2.py
+++ clpipe/fmri_postprocess2.py
@@ -16,7 +16,7 @@ from .error_handler import exception_handler
 # This hides a pybids warning
 bids_config.set_option('extension_initial_dot', True)
 
-logging.basicConfig()
+# logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 class NoSubjectsFoundError(ValueError):
@@ -155,6 +155,8 @@ class PostProcessSubjectJob():
         self.out_dir = out_dir
         self.config_file = config_file
 
+        self.setup_logger()
+
     def __str__(self):
         return f"Postprocessing Job: sub-{self.subject_id}"
 
@@ -163,74 +165,119 @@ class PostProcessSubjectJob():
         # doesn't already exist
         self.subject_out_dir = self.out_dir / ("sub-" + self.subject_id) / "func"
         if not self.subject_out_dir.exists():
+            self.logger.info(f"Creating subject directory: {self.subject_out_dir}")
             self.subject_out_dir.mkdir(exist_ok=True, parents=True)
 
         # Create a nipype working directory for this subject, if it doesn't exist
         self.working_dir = self.subject_out_dir / "working"
         if not self.working_dir.exists():
+            self.logger.info(f"Creating subject working directory: {self.working_dir}")
             self.subject_out_dir.mkdir(exist_ok=True)
 
         # Create a postprocessing logging directory for this subject, if it doesn't exist
         self.log_dir = self.log_dir / ("sub-" + self.subject_id)
         if not self.log_dir.exists():
+            self.logger.info(f"Creating subject log directory: {self.log_dir}")
             self.log_dir.mkdir(exist_ok=True)
 
     def get_confounds(self):
         # Find the subject's confounds file
-        # TODO: Need switch here from config to determine if confounds wanted
+        
+        self.logger.info("Searching for confounds file")
         try:
             self.confounds = self.bids.get(
                 subject=self.subject_id, suffix="timeseries", extension=".tsv"
             )[0]
+            self.logger.info(f"Confounds file found: {self.confounds}")
         except IndexError:
-            LOG.info(f"Confounds file for subject {self.subject_id} not found.")
+            self.logger.info(f"Confounds file for subject {self.subject_id} not found.")
             self.confounds = None
 
     def get_mask(self):
         # Find the subject's mask file
-        # TODO: Need switch here from config to determine if mask file wanted
+        self.logger.info("Searching for mask file")
         try:
             self.mask_image = self.bids.get(
                 subject=self.subject_id, suffix="mask", extension=".nii.gz"
             )[0]
+            self.logger.info(f"Mask file found: {self.mask_image}")
             #TODO: Throw multiple masks found exception?
         except IndexError:
-            LOG.info(f"Mask image for subject {self.subject_id} not found.")
+            self.logger.info(f"Mask image for subject {self.subject_id} not found.")
             self.mask_image = None
 
     def get_images_to_process(self):
+        self.logger.info("Searching for images to process")
         # Find the subject's images to run post_proc on
         self.images_to_process = self.bids.get(
             subject=self.subject_id, return_type="filename", 
             extension="nii.gz", datatype="func", suffix="bold")
+        self.logger.info(f"Found {len(self.images_to_process)} images to process")
+
+    def setup_logger(self):
+        self.logger = logging.getLogger(f"{self.__class__.__name__}.sub-{self.subject_id}")
+        self.logger.setLevel(logging.INFO)
+        
+        # Create log handler
+        c_handler = logging.StreamHandler()
+        c_handler.setLevel(logging.INFO)
+
+        # Create log formatter to add to handler
+        c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        c_handler.setFormatter(c_format)
+        
+        # Add handler to logger
+        self.logger.addHandler(c_handler)
+
+    def setup_file_logger(self):
+        # Create log handler
+        f_handler = logging.FileHandler(self.log_dir / f'sub-{self.subject_id}.log')
+        f_handler.setLevel(logging.DEBUG)
+        
+        # Create log format
+        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        f_handler.setFormatter(f_format)
+
+        # Add handler to the logger
+        self.logger.addHandler(f_handler)
 
     def run(self):
+        self.logger.info(f"Running postprocessing for sub-{self.subject_id}")
+
         # Get postprocessing configuration from general config
+        self.logger.info(f"Ingesting configuration: {self.config_file}")
         configParser = ClpipeConfigParser()
         configParser.config_updater(self.config_file)
         self.postprocessing_config = configParser.config["PostProcessingOptions2"]
 
         # Open the fmriprep dir and validate that it contains the subject
+        self.logger.info(f"Checking fmrioutput for requested subject in: {self.fmriprep_dir}")
         try:
             self.bids:BIDSLayout = _get_bids_dir(self.fmriprep_dir, validate=False, index_metadata=False)
 
             if len(self.bids.get(subject=self.subject_id)) == 0:
                 snfe = f"Subject {self.subject_id} was not found in fmriprep directory {self.fmriprep_dir}"
-                LOG.error(snfe)
+                self.logger.error(snfe)
                 raise SubjectNotFoundError(snfe)
         except FileNotFoundError as fne:
-            fnfe = f"Invalid fmriprep output path provided: {fmriprep_dir}"
-            LOG.error(fnfe)
+            fnfe = f"Invalid fmriprep output path provided: {self.fmriprep_dir}"
+            self.logger.error(fnfe, exc_info=True)
             raise fne
         
         self.setup_directories()
+        self.setup_file_logger()
+        # TODO: Need switch here from config to determine if confounds wanted
         self.get_confounds()
+        # TODO: Need switch here from config to determine if mask file wanted
         self.get_mask()
         self.get_images_to_process()
 
         # Process the subject's confounds
+        # TODO: Need switch here from config to determine if confounds wanted
+        self.logger.info("Preparing confounds")
         prepare_confounds(Path(self.confounds), self.subject_out_dir / "confounds.tsv",
             self.postprocessing_config["ConfoundOptions"])
+
 
         # Process the subject's images
         for in_file in self.images_to_process:
@@ -239,16 +286,20 @@ class PostProcessSubjectJob():
             out_stem = image_name + '_postproccessed.nii.gz'
             out_file = os.path.abspath(os.path.join(self.subject_out_dir, out_stem))
 
+            self.logger.info(f"Building postprocessing workflow for image: {in_file}")
             self.wf = build_postprocessing_workflow(self.postprocessing_config, in_file, out_file, self.postprocessing_config["ImageTR"], 
                 name="sub_" + self.subject_id, mask_file=self.mask_image,
                 base_dir=self.working_dir, crashdump_dir=self.log_dir)
 
-            LOG.info(f"Postprocessing image at path {in_file}")
+            self.logger.info(f"Running postprocessing workflow for image: {in_file}")
             self.wf.run()
+            self.logger.info(f"Postprocessing workflow complete for image: {in_file}")
 
             # Draw the workflow's process graph if requested in config
             if self.postprocessing_config["WriteProcessGraph"]:
-                self.wf.write_graph(dotfilename = self.subject_out_dir / "process_graph.dot" , graph2use="colored")
+                graph_image_path = self.subject_out_dir / "process_graph.dot"
+                self.logger.info(f"Drawing workflow graph: {graph_image_path}")
+                self.wf.write_graph(dotfilename = graph_image_path, graph2use="colored")
 
 
 def _get_subjects(fmriprep_dir: BIDSLayout, subjects):   
@@ -270,6 +321,8 @@ class PostProcessSubjectJobs():
     def __init__(self, fmriprep_dir, output_dir: os.PathLike, config_file: os.PathLike, 
         subjects_to_process=None, log_dir: os.PathLike=None, pybids_db_path: os.PathLike=None):
         
+        self.setup_logger()
+
         self.output_dir = output_dir
 
         # Create the root output directory for all subject postprocessing results, if it doesn't yet exist.
@@ -291,13 +344,17 @@ class PostProcessSubjectJobs():
         self.create_jobs()
 
     def create_jobs(self):
+        self.logger.info("Creating postprocessing jobs")
         for subject in self.subjects_to_process:
             # Create a new job and add to list of jobs to be run
             job_to_add = PostProcessSubjectJob(subject, self.fmriprep_dir,
                 self.output_dir, self.config_file, log_dir=self.log_dir)
             self.post_process_jobs.append(job_to_add)
+        self.logger.info(f"Created {len(self.post_process_jobs)} postprocessing jobs")
         
     def set_batch_manager(self, batch_manager: BatchManager):
+        self.logger.info("Setting up batch management")
+        
         self.slurm=True
         self.batch_manager = batch_manager
 
@@ -321,18 +378,52 @@ class PostProcessSubjectJobs():
     def setup_directories(self):
         # Create a postproc output directory, if it doesn't exist
         if not self.output_dir.exists():
+            self.logger.info(f"Creating postprocessing directory: {self.output_dir}")
             self.output_dir.mkdir(exist_ok=True, parents=True)
 
         # Create a postproc log directory, if it doesn't exist
         if not self.log_dir.exists():
+            self.logger.info(f"Creating postprocessing log directory at {self.log_dir}")
             self.log_dir.mkdir(exist_ok=True)
+
+    def setup_logger(self):
+        self.logger = logging.getLogger(f"{self.__class__.__name__}")
+        self.logger.setLevel(logging.INFO)
+        
+        # Create log handler
+        c_handler = logging.StreamHandler()
+        c_handler.setLevel(logging.INFO)
+
+        # Create log formatter to add to handler
+        c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        c_handler.setFormatter(c_format)
+        
+        # Add handler to logger
+        self.logger.addHandler(c_handler)
+
+    def setup_file_logger(self):
+        # Create log handler
+        f_handler = logging.FileHandler(self.log_dir / f'postprocess.log')
+        f_handler.setLevel(logging.DEBUG)
+        
+        # Create log format
+        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        f_handler.setFormatter(f_format)
+
+        # Add handler to the logger
+        self.logger.addHandler(f_handler)
 
     def run(self):
         self.setup_directories()
+        self.setup_file_logger()
+        
+        num_jobs = len(self.post_process_jobs)
 
         if self.slurm:
+            self.logger.info(f"Running {num_jobs} postprocessing jobs in batch mode")
             self.batch_manager.submit_jobs()
         else:
+            self.logger.info(f"Running {num_jobs} postprocessing jobs")
             for job in self.post_process_jobs:
                 job.run()
         
