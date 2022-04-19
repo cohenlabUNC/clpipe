2f1af780c8e60f88ba4cc98a7939d176640a09f1
Will Asciutto <wasciutto@unc.edu>
2022-04-19 16:24:21 -0400

    Add a system exit if required parameter not given.

--- clpipe/cli.py
+++ clpipe/cli.py
@@ -194,6 +194,7 @@ def glm_apply_mumford_workaround(glm_config_file, l1_feat_folders_path):
     """Apply the Mumford registration workaround to L1 FEAT folders. Applied by default in glm-l2-preparefsf. """
     if not (glm_config_file or l1_feat_folders_path):
         click.echo("Error: At least one of either option '-glm_config_file' or '-l1_feat_folders_path' required.")
+        sys.exit()
 
 
 @cli.command()
