6f341bb4fc65ef1eba6af9517e26a96a5d63b826
Will Asciutto <wasciutto@unc.edu>
2022-07-18 10:56:34 -0400

    Add more logging.

--- clpipe/dcm2bids_wrapper.py
+++ clpipe/dcm2bids_wrapper.py
@@ -1,13 +1,17 @@
-import click
-from .batch_manager import BatchManager,Job
+from .batch_manager import BatchManager, Job
 from .config_json_parser import ClpipeConfigParser
 import os
 import parse
 import glob
-from .error_handler import exception_handler
 import sys
 
-def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, conv_config_file=None, config_file=None, overwrite=None, log_dir=None, subject=None, session=None, longitudinal=False, submit=None):
+from .utils import get_logger, add_file_handler
+
+def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, 
+                 conv_config_file=None, config_file=None, overwrite=None, 
+                 log_dir=None, subject=None, session=None, longitudinal=False, 
+                 submit=None, debug=False):
+    
     config = ClpipeConfigParser()
     config.config_updater(config_file)
     config.setup_dcm2bids(dicom_dir,
@@ -16,32 +20,42 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, conv_conf
                           dicom_dir_format,
                           log_dir)
 
+    add_file_handler(os.path.join(config.config["ProjectDirectory"], "logs"))
+    logger = get_logger("bids-conversion", debug=debug)
+    
     if not config.config['DICOMToBIDSOptions']['DICOMDirectory']:
-        raise ValueError('DICOM directory not specified.')
+        logger.error('DICOM directory not specified.')
+        sys.exit(1)
     if not config.config['DICOMToBIDSOptions']['BIDSDirectory']:
-        raise ValueError('BIDS directory not specified.')
+        logger.error('BIDS directory not specified.')
+        sys.exit(1)
     if not config.config['DICOMToBIDSOptions']['ConversionConfig']:
-        raise ValueError('Conversion config not specified.')
+        logger.error('Conversion config not specified.')
+        sys.exit(1)
     if not config.config['DICOMToBIDSOptions']['DICOMFormatString']:
-        raise ValueError('Format string not specified.')
+        logger.error('Format string not specified.')
+        sys.exit(1)
     if not config.config['DICOMToBIDSOptions']['LogDirectory']:
-        raise ValueError('Log directory not specified.')
-
+        logger.error('Log directory not specified.')
+        sys.exit(1)
 
     dicom_dir = config.config['DICOMToBIDSOptions']['DICOMDirectory']
     dicom_dir_format = config.config['DICOMToBIDSOptions']['DICOMFormatString']
 
+    logger.info(f"Starting bids conversion targeting: {dicom_dir}")
 
-    formatStr = dicom_dir_format.replace("{subject}", "*")
+    format_str = dicom_dir_format.replace("{subject}", "*")
     session_toggle = False
     if "{session}" in dicom_dir_format:
         session_toggle = True
 
-    formatStr = formatStr.replace("{session}", "*")
-    click.echo(formatStr)
+    format_str = format_str.replace("{session}", "*")
+    logger.debug(f"Format string: {format_str}")
+
     pstring = os.path.join(dicom_dir, dicom_dir_format+'/')
-    click.echo(pstring)
-    folders = glob.glob(os.path.join(dicom_dir, formatStr+'/'))
+    logger.debug(f"pstring: {pstring}")
+    
+    folders = glob.glob(os.path.join(dicom_dir, format_str+'/'))
     sub_sess_list = [parse.parse(pstring, x) for x in folders]
     sub_inds = [ind for ind, x in enumerate(sub_sess_list)]
     sess_inds = [ind for ind, x in enumerate(sub_sess_list)]
@@ -55,8 +69,9 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, conv_conf
     folders = [folders[i] for i in sub_sess_inds]
     sub_sess_list = [sub_sess_list[i] for i in sub_sess_inds]
     if len(sub_sess_list) == 0:
-        sys.excepthook = exception_handler
-        raise FileNotFoundError('There are no subjects/sessions found for that format string.')
+        logger.error((f'There are no subjects/sessions found for format '
+                       'string: {format_str}'))
+        sys.exit(1)
 
     if session_toggle and not longitudinal:
         conv_string = '''dcm2bids -d {dicom_dir} -o {bids_dir} -p {subject} -s {session} -c {conv_config_file}'''
@@ -66,7 +81,11 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, conv_conf
     if overwrite:
         conv_string = conv_string + " --clobber --forceDcm2niix"
 
-    batch_manager = BatchManager(config.config['BatchConfig'], config.config['DICOMToBIDSOptions']['LogDirectory'])
+    batch_manager = BatchManager(
+        config.config['BatchConfig'], 
+        config.config['DICOMToBIDSOptions']['LogDirectory'],
+        debug=debug
+    )
     batch_manager.createsubmissionhead()
     batch_manager.update_mem_usage(config.config['DICOMToBIDSOptions']['MemUsage'])
     batch_manager.update_time(config.config['DICOMToBIDSOptions']['TimeUsage'])
@@ -103,8 +122,10 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, conv_conf
     batch_manager.compilejobstrings()
     if submit:
         batch_manager.submit_jobs()
-        config.config_json_dump(os.path.dirname(os.path.abspath(config_file)), config_file)
+        config.config_json_dump(os.path.dirname(os.path.abspath(config_file)),
+                                config_file)
     else:
         batch_manager.print_jobs()
+        logger.info("Rerun with the '-submit' flag to launch these jobs.")
 
 
