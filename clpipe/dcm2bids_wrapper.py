bdece3ae6c7ee1c45202c593abb4a20762374653
Will Asciutto <wasciutto@unc.edu>
2022-07-18 15:39:31 -0400

    Clean up dcm2bids_wrapper.

--- clpipe/dcm2bids_wrapper.py
+++ clpipe/dcm2bids_wrapper.py
@@ -7,6 +7,9 @@ import sys
 
 from .utils import get_logger, add_file_handler
 
+BASE_CMD = ("dcm2bids -d {dicom_dir} -o {bids_dir} "
+            "-p {subject} -c {conv_config_file}")
+
 def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None, 
                  conv_config_file=None, config_file=None, overwrite=None, 
                  log_dir=None, subject=None, session=None, longitudinal=False, 
@@ -20,27 +23,35 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None,
                           dicom_dir_format,
                           log_dir)
 
-    add_file_handler(os.path.join(config.config["ProjectDirectory"], "logs"))
-    logger = get_logger("bids-conversion", debug=debug)
-    
-    if not config.config['DICOMToBIDSOptions']['DICOMDirectory']:
-        logger.error('DICOM directory not specified.')
-        sys.exit(1)
-    if not config.config['DICOMToBIDSOptions']['BIDSDirectory']:
-        logger.error('BIDS directory not specified.')
-        sys.exit(1)
-    if not config.config['DICOMToBIDSOptions']['ConversionConfig']:
-        logger.error('Conversion config not specified.')
-        sys.exit(1)
-    if not config.config['DICOMToBIDSOptions']['DICOMFormatString']:
-        logger.error('Format string not specified.')
-        sys.exit(1)
-    if not config.config['DICOMToBIDSOptions']['LogDirectory']:
-        logger.error('Log directory not specified.')
-        sys.exit(1)
-
+    project_dir = config.config["ProjectDirectory"]
     dicom_dir = config.config['DICOMToBIDSOptions']['DICOMDirectory']
     dicom_dir_format = config.config['DICOMToBIDSOptions']['DICOMFormatString']
+    bids_dir = config.config['DICOMToBIDSOptions']['BIDSDirectory']
+    conv_config = config.config['DICOMToBIDSOptions']['ConversionConfig']
+    log_dir = config.config['DICOMToBIDSOptions']['LogDirectory']
+    batch_config = config.config['BatchConfig']
+    mem_usage = config.config['DICOMToBIDSOptions']['MemUsage']
+    time_usage = config.config['DICOMToBIDSOptions']['TimeUsage']
+    n_threads = config.config['DICOMToBIDSOptions']['CoreUsage']
+
+    add_file_handler(os.path.join(project_dir, "logs"))
+    logger = get_logger("bids-conversion", debug=debug)
+
+    if not dicom_dir:
+        logger.error('DICOM directory not specified.')
+        sys.exit(1)
+    if not bids_dir:
+        logger.error('BIDS directory not specified.')
+        sys.exit(1)
+    if not conv_config:
+        logger.error('Conversion config not specified.')
+        sys.exit(1)
+    if not dicom_dir_format:
+        logger.error('Format string not specified.')
+        sys.exit(1)
+    if not log_dir:
+        logger.error('Log directory not specified.')
+        sys.exit(1)
 
     logger.info(f"Starting bids conversion targeting: {dicom_dir}")
 
@@ -59,11 +70,13 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None,
     sub_sess_list = [parse.parse(pstring, x) for x in folders]
     sub_inds = [ind for ind, x in enumerate(sub_sess_list)]
     sess_inds = [ind for ind, x in enumerate(sub_sess_list)]
+    
     if subject is not None:
-        sub_inds = [ind for ind, x in enumerate(sub_sess_list) if x['subject'] == subject]
-
+        sub_inds = [ind for ind, x in enumerate(sub_sess_list) \
+            if x['subject'] == subject]
     if session is not None:
-        sess_inds = [ind for ind, x in enumerate(sub_sess_list) if x['session'] == session]
+        sess_inds = [ind for ind, x in enumerate(sub_sess_list) \
+            if x['session'] == session]
 
     sub_sess_inds = list(set(sub_inds) & set(sess_inds))
     folders = [folders[i] for i in sub_sess_inds]
@@ -73,50 +86,51 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None,
                        'string: {format_str}'))
         sys.exit(1)
 
+    conv_string = BASE_CMD
     if session_toggle and not longitudinal:
-        conv_string = '''dcm2bids -d {dicom_dir} -o {bids_dir} -p {subject} -s {session} -c {conv_config_file}'''
-    else:
-        conv_string = '''dcm2bids -d {dicom_dir} -o {bids_dir} -p {subject} -c {conv_config_file}'''
+        conv_string += " -s {session}"
 
     if overwrite:
         conv_string = conv_string + " --clobber --forceDcm2niix"
 
-    batch_manager = BatchManager(
-        config.config['BatchConfig'], 
-        config.config['DICOMToBIDSOptions']['LogDirectory'],
-        debug=debug
-    )
+    batch_manager = BatchManager(batch_config, log_dir, debug=debug)
     batch_manager.createsubmissionhead()
-    batch_manager.update_mem_usage(config.config['DICOMToBIDSOptions']['MemUsage'])
-    batch_manager.update_time(config.config['DICOMToBIDSOptions']['TimeUsage'])
-    batch_manager.update_nthreads(config.config['DICOMToBIDSOptions']['CoreUsage'])
-    for ind,i in enumerate(sub_sess_list):
+    batch_manager.update_mem_usage(mem_usage)
+    batch_manager.update_time(time_usage)
+    batch_manager.update_nthreads(n_threads)
 
+    for ind,i in enumerate(sub_sess_list):
         if session_toggle and not longitudinal:
-             job_id = 'convert_sub-' + i['subject'] + '_ses-' + i['session']
-             job1 = Job(job_id, conv_string.format(
-                dicom_dir=folders[ind],
-                subject = i['subject'],
-                session =i['session'],
-                conv_config_file = config.config['DICOMToBIDSOptions']['ConversionConfig'],
-                bids_dir = config.config['DICOMToBIDSOptions']['BIDSDirectory']
-            ))
+            job_id = 'convert_sub-' + i['subject'] + '_ses-' + i['session']
+            job1 = Job(
+                job_id, conv_string.format(
+                    dicom_dir = folders[ind],
+                    subject = i['subject'],
+                    session = i['session'],
+                    conv_config_file = conv_config,
+                    bids_dir = bids_dir
+                )
+            )
         elif longitudinal:
             job_id = 'convert_sub-' + i['subject']+ '_ses-' + i['session']
-            job1 = Job(job_id, conv_string.format(
-                dicom_dir=folders[ind],
-                subject=i['subject'] + "sess"+ i['session'],
-                conv_config_file=config.config['DICOMToBIDSOptions']['ConversionConfig'],
-                bids_dir=config.config['DICOMToBIDSOptions']['BIDSDirectory']
-            ))
+            job1 = Job(
+                job_id, conv_string.format(
+                    dicom_dir = folders[ind],
+                    subject = i['subject'] + "sess"+ i['session'],
+                    conv_config_file = conv_config,
+                    bids_dir = bids_dir
+                )
+            )
         else:
             job_id = 'convert_sub-' + i['subject']
-            job1 = Job(job_id, conv_string.format(
-                dicom_dir=folders[ind],
-                subject=i['subject'],
-                conv_config_file=config.config['DICOMToBIDSOptions']['ConversionConfig'],
-                bids_dir=config.config['DICOMToBIDSOptions']['BIDSDirectory']
-            ))
+            job1 = Job(
+                job_id, conv_string.format(
+                    dicom_dir = folders[ind],
+                    subject = i['subject'],
+                    conv_config_file = conv_config,
+                    bids_dir = bids_dir
+                )
+            )
         batch_manager.addjob(job1)
 
     batch_manager.compilejobstrings()
@@ -127,5 +141,3 @@ def convert2bids(dicom_dir=None, dicom_dir_format=None, bids_dir=None,
     else:
         batch_manager.print_jobs()
         logger.info("Rerun with the '-submit' flag to launch these jobs.")
-
-
