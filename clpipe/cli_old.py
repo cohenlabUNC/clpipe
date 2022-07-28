433727c03e16605dd1191f5b026f5af2bd6cc685
Will Asciutto <wasciutto@unc.edu>
2022-07-28 11:26:50 -0400

    Move postproc2 cli commands back to module.

--- clpipe/cli_old.py
+++ clpipe/cli_old.py
@@ -1,89 +1,12 @@
 import click
 import sys
 
-from .fmri_postprocess2 import postprocess_image_controller,\
-    postprocess_subject_controller, postprocess_subjects_controller,\
-    DEFAULT_PROCESSING_STREAM_NAME
 from .glm_setup import glm_setup as glm_setup_logic
 from .glm_l1 import glm_l1_preparefsf as glm_l1_preparefsf_logic,\
     glm_l1_launch_controller
 from .glm_l2 import glm_l2_preparefsf as glm_l2_preparefsf_logic
 from .fsl_onset_extract import fsl_onset_extract as fsl_onset_extract_logic
 from .outliers_report import get_study_outliers, get_image_confounds
-
-
-
-
-
-@cli.command()
-@click.argument('subjects', nargs=-1, required=False, default=None)
-@click.option('-config_file', type=click.Path(exists=True, dir_okay=False, file_okay=True), default=None, required=True,
-              help='Use a given configuration file.')
-@click.option('-fmriprep_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False), help="""Which fmriprep directory to process. 
-    If a configuration file is provided with a BIDS directory, this argument is not necessary. 
-    Note, must point to the ``fmriprep`` directory, not its parent directory.""")
-@click.option('-output_dir', type=click.Path(dir_okay=True, file_okay=False), default=None, required=False, help = """Where to put the postprocessed data. 
-    If a configuration file is provided with a output directory, this argument is not necessary.""")
-@click.option('-processing_stream', default=DEFAULT_PROCESSING_STREAM_NAME, required=False, help="Specify a processing stream to use defined in your configuration file.")
-@click.option('-log_dir', type=click.Path(exists=True, dir_okay=True, file_okay=False), default=None, required = False, help = 'Path to the logging directory.')
-@click.option('-index_dir', type=click.Path(dir_okay=True, file_okay=False), default=None, required=False,
-              help='Give the path to an existing pybids index database.')
-@click.option('-refresh_index', is_flag=True, default=False, required=False,
-              help='Refresh the pybids index database to reflect new fmriprep artifacts.')
-@click.option('-batch/-no-batch', is_flag = True, default=True, help = 'Flag to create batch jobs without prompt.')
-@click.option('-submit', is_flag = True, default=False, help = 'Flag to submit commands to the HPC without prompt.')
-@click.option('-debug', is_flag = True, default=False, help = 'Print detailed processing information and traceback for errors.')
-def fmri_postprocess2(subjects, config_file, fmriprep_dir, output_dir, 
-                      processing_stream, batch, submit, log_dir, index_dir, 
-                      refresh_index, debug):
-    """Perform additional processing on fMRIPrepped data"""
-
-    postprocess_subjects_controller(
-        subjects=subjects, config_file=config_file,fmriprep_dir=fmriprep_dir, 
-        output_dir=output_dir, processing_stream=processing_stream,
-        batch=batch, submit=submit, log_dir=log_dir, pybids_db_path=index_dir,
-        refresh_index=refresh_index, debug=debug)
-
-
-@click.command()
-@click.argument('subject_id')
-@click.argument('bids_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('fmriprep_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('output_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('processing_stream', default=DEFAULT_PROCESSING_STREAM_NAME)
-@click.argument('config_file', type=click.Path(dir_okay=False, file_okay=True))
-@click.argument('index_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.option('-batch/-no-batch', is_flag = True, default=True, help = 'Flag to create batch jobs without prompt.')
-@click.option('-submit', is_flag = True, default=False, help = 'Flag to submit commands to the HPC without prompt.')
-@click.argument('log_dir', type=click.Path(dir_okay=True, file_okay=False))
-def postprocess_subject_cli(subject_id, bids_dir, fmriprep_dir, output_dir, 
-                            processing_stream, config_file, index_dir, 
-                            batch, submit, log_dir):
-    
-    postprocess_subject_controller(
-        subject_id, bids_dir, fmriprep_dir, output_dir, config_file, index_dir, 
-        batch, submit, log_dir, processing_stream=processing_stream)
-
-
-@click.command()
-@click.argument('config_file', type=click.Path(dir_okay=False, file_okay=True))
-@click.argument('image_path', type=click.Path(dir_okay=False, file_okay=True))
-@click.argument('bids_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('fmriprep_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('index_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('out_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('subject_out_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('processing_stream', default=DEFAULT_PROCESSING_STREAM_NAME)
-@click.argument('subject_working_dir', type=click.Path(dir_okay=True, file_okay=False))
-@click.argument('log_dir', type=click.Path(dir_okay=True, file_okay=False))
-def postprocess_image_cli(config_file, image_path, bids_dir, fmriprep_dir, 
-                          index_dir, out_dir, subject_out_dir, 
-                          processing_stream, subject_working_dir, log_dir):
-    
-    postprocess_image_controller(
-        config_file, image_path, bids_dir, fmriprep_dir, index_dir, out_dir, 
-        subject_out_dir, subject_working_dir, log_dir, 
-        processing_stream=processing_stream)
 
 
 @cli.command()
