65a54614a34085a09195c3d67e57581da2f32298
Will Asciutto <wasciutto@unc.edu>
2022-08-05 18:21:19 -0400

    Move application name to config.

--- clpipe/__init__.py
+++ clpipe/__init__.py
@@ -1,11 +1,13 @@
 import logging
 
+from .config import APPLICATION_NAME
+
 # Initialize logging config - this must be present for logging messages to work
 logging.basicConfig(level=logging.WARNING, 
     format='%(message)s')
 
 # Initalize the main clpipe logger with 'INFO' level default
-logger = logging.getLogger("clpipe")
+logger = logging.getLogger(APPLICATION_NAME)
 logger.setLevel(logging.INFO)
 
 # Clear the main logger's handlers to avoid double printing when sub loggers
