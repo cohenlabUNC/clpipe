4d20e23ebca5b19eceb293dae363ccbfe40ab48c
Bhavith <catchbhav2002@gmail.com>
2022-10-06 16:38:31 -0400

    fixed build script. Need to test docs

--- build_clpipe.sh
+++ build_clpipe.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # A simple script to build clpipe and generate the documentation
 
-cd clpipe
-mkdir clpipe-build
-cd clpipe-build
-mkdir data_DICOMs
-cd ..
-python -c'from clpipe import project_setup;import os; project_setup.project_setup("clpipe",os.getcwd(),"/data_DICOMs")'
-cd ..
 python -m build --wheel
-cd docs
-make html
+(cd docs; make html)
