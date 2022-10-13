616351c6e11adb67914917b79f7c63bf03c50a95
Will Asciutto <wasciutto@unc.edu>
2022-10-13 10:13:17 -0400

    Add DICOM conversion tests.

--- tests/bids/test_conversion.py
+++ tests/bids/test_conversion.py
@@ -1,8 +1,53 @@
 import pytest
 
-from clpipe.bids_conversion import dcm2bids_wrapper
+from clpipe.bids_conversion import convert2bids
 
-def test_dcm2bids(clpipe_dir):
-    dicom_dir = clpipe_dir / "data_DICOMs"
+def test_dcm2bids(clpipe_dicom_dir, config_file):
+    convert2bids(
+        dcm2bids = True,
+        config_file = config_file,
+        dicom_dir = clpipe_dicom_dir / "sub",
+        dicom_dir_format = "{subject}"
+    )
+
+    assert True
+
+def test_dcm2bids_sub_session(clpipe_dicom_dir, config_file):
+    convert2bids(
+        dcm2bids = True,
+        config_file = config_file,
+        dicom_dir = clpipe_dicom_dir / "sub_session",
+        dicom_dir_format = "{subject}/{session}"
+    )
+
+    assert True
+
+def test_dcm2bids_sub_session_flat(clpipe_dicom_dir, config_file):
+    convert2bids(
+        dcm2bids = True,
+        config_file = config_file,
+        dicom_dir = clpipe_dicom_dir / "sub_session_flat",
+        dicom_dir_format = "{subject}_{session}"
+    )
+
+    assert True
+
+def test_dcm2bids_session_sub(clpipe_dicom_dir, config_file):
+    convert2bids(
+        dcm2bids = True,
+        config_file = config_file,
+        dicom_dir = clpipe_dicom_dir / "session_sub",
+        dicom_dir_format = "{session}/{subject}"
+    )
+
+    assert True
+
+def test_dcm2bids_session_sub_flat(clpipe_dicom_dir, config_file):
+    convert2bids(
+        dcm2bids = True,
+        config_file = config_file,
+        dicom_dir = clpipe_dicom_dir / "session_sub_flat",
+        dicom_dir_format = "{session}_{subject}"
+    )
 
     assert True
