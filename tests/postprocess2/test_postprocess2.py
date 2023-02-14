b10898ff4e2057153236018376f6082fc55839f2
Will Asciutto <wasciutto@unc.edu>
2023-02-14 13:04:34 -0500

    Fix all references in postprocess2 tests.

--- tests/postprocess2/test_postprocess2.py
+++ tests/postprocess2/test_postprocess2.py
@@ -5,8 +5,8 @@ import traceback
 from click.testing import CliRunner
 
 from clpipe.postprocutils.workflows import *
-from clpipe.fmri_postprocess2 import PostProcessSubjectJobs, PostProcessSubjectJob, postprocess_fmriprep_dir, fmri_postprocess2_cli
-
+from clpipe.fmri_postprocess2 import *
+from clpipe.cli import fmri_postprocess2_cli
 
 def test_postprocess_cli_debug(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     """Note: this test always passes because click does its own exit code handling - but this lets one trace through the cli with a debugger"""
@@ -27,6 +27,7 @@ def test_postprocess_cli_debug(clpipe_fmriprep_dir, artifact_dir, helpers, reque
                                 '-log_dir', str(log_dir),
                                 '-no-batch', '-debug'])
 
+
 def test_postprocess_cli(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
     config = clpipe_fmriprep_dir / "clpipe_config.json"
@@ -38,7 +39,7 @@ def test_postprocess_cli(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     
     runner = CliRunner()
     result = runner.invoke(
-        hngprep_cli, 
+        fmri_postprocess2_cli, 
         ['-config_file', str(config),
         '-target_dir', str(fmriprep_dir),
         '-output_dir', str(postproc_dir),
@@ -49,7 +50,8 @@ def test_postprocess_cli(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     traceback.print_exc(result.exception)
     assert result.stderr == 0
 
-def test_postprocess_fmriprep_dir(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+
+def test_postprocess_subjects_dir(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
     config = clpipe_fmriprep_dir / "clpipe_config.json"
     glm_config = clpipe_fmriprep_dir / "glm_config.json"
@@ -58,16 +60,18 @@ def test_postprocess_fmriprep_dir(clpipe_fmriprep_dir, artifact_dir, helpers, re
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    postprocess_fmriprep_dir(config_file=config, glm_config_file=glm_config, fmriprep_dir=fmriprep_dir,
+    postprocess_subjects(config_file=config, glm_config_file=glm_config, fmriprep_dir=fmriprep_dir,
         output_dir=postproc_dir, log_dir=log_dir)
 
-def test_postprocess_fmriprep_dir_config_only(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+
+def test_postprocess_subjects_dir_config_only(clpipe_fmriprep_dir):
     config = clpipe_fmriprep_dir / "clpipe_config.json"
 
     with pytest.raises(SystemExit):
-        postprocess_fmriprep_dir(config_file=config, submit=True, batch=False)
+        postprocess_subjects(config_file=config, submit=True, batch=False)
 
-def test_postprocess_fmriprep_dir_invalid_subject(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+
+def test_postprocess_subjects_dir_invalid_subject(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
     config = clpipe_fmriprep_dir / "clpipe_config.json"
     glm_config = clpipe_fmriprep_dir / "glm_config.json"
@@ -76,8 +80,9 @@ def test_postprocess_fmriprep_dir_invalid_subject(clpipe_fmriprep_dir, artifact_
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    postprocess_fmriprep_dir(subjects=['99'], config_file=config, glm_config_file=glm_config, fmriprep_dir=fmriprep_dir,
+    postprocess_subjects(subjects=['99'], config_file=config, glm_config_file=glm_config, fmriprep_dir=fmriprep_dir,
         output_dir=postproc_dir, log_dir=log_dir)
+
 
 def test_postprocess2_wf_2_steps(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_confounds_timeseries, plot_img, write_graph, helpers):
@@ -101,6 +106,7 @@ def test_postprocess2_wf_2_steps(artifact_dir, postprocessing_config, request, s
 
     assert True
 
+
 def test_postprocess2_wf_3_steps(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_confounds_timeseries, plot_img, write_graph, helpers):
 
@@ -123,6 +129,7 @@ def test_postprocess2_wf_3_steps(artifact_dir, postprocessing_config, request, s
 
     assert True
 
+
 def test_postprocess2_wf_1_step(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_confounds_timeseries, plot_img, write_graph, helpers):
 
@@ -144,6 +151,7 @@ def test_postprocess2_wf_1_step(artifact_dir, postprocessing_config, request, sa
         helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
+
 
 # This test won't work until properly processed confound file provided
 def test_postprocess2_wf_confound_regression_last(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
@@ -168,6 +176,7 @@ def test_postprocess2_wf_confound_regression_last(artifact_dir, postprocessing_c
 
     assert True
 
+
 def test_postprocess2_wf_confound_regression_first(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_confounds_timeseries, plot_img, write_graph, helpers):
 
@@ -189,6 +198,7 @@ def test_postprocess2_wf_confound_regression_first(artifact_dir, postprocessing_
         helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
+
 
 def test_postprocess2_wf_aroma(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_melodic_mixing, sample_aroma_noise_ics, plot_img, write_graph, helpers):
@@ -212,6 +222,7 @@ def test_postprocess2_wf_aroma(artifact_dir, postprocessing_config, request, sam
 
     assert True
 
+
 def test_postprocess2_wf_aroma_last(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_melodic_mixing, sample_aroma_noise_ics, plot_img, write_graph, helpers):
 
@@ -234,6 +245,7 @@ def test_postprocess2_wf_aroma_last(artifact_dir, postprocessing_config, request
 
     assert True
 
+
 def test_postprocess2_wf_no_mask(artifact_dir, postprocessing_config, request, sample_raw_image, 
     plot_img, write_graph, helpers):
     test_path = helpers.create_test_dir(artifact_dir, request.node.name)
@@ -252,6 +264,7 @@ def test_postprocess2_wf_no_mask(artifact_dir, postprocessing_config, request, s
 
     assert True
 
+
 def test_postprocess_subjects_job(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     config = clpipe_fmriprep_dir / "clpipe_config.json"
     bids_dir = clpipe_fmriprep_dir / "data_BIDS"
@@ -264,22 +277,21 @@ def test_postprocess_subjects_job(clpipe_fmriprep_dir, artifact_dir, helpers, re
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    jobs = PostProcessSubjectJobs(bids_dir, fmriprep_dir, postproc_dir, config,
+    postprocess_subjects(bids_dir, fmriprep_dir, postproc_dir, config,
         log_dir=log_dir, pybids_db_path=pybids_db_path)
-    jobs.run()
 
-    assert len(jobs.post_process_jobs) == 8
 
 def test_prepare_confounds(sample_confounds_timeseries, postprocessing_config, artifact_dir, helpers, request):
     test_path = helpers.create_test_dir(artifact_dir, request.node.name)
     out_path = test_path / "new_confounds.tsv"
 
-    cf_workflow = build_confound_postprocessing_workflow(postprocessing_config, confound_file=sample_confounds_timeseries,
+    cf_workflow = build_confounds_processing_workflow(postprocessing_config, confound_file=sample_confounds_timeseries,
         out_file=out_path, base_dir=test_path, crashdump_dir=test_path, tr=2)
 
     cf_workflow.run()
     
     assert True
+
 
 def test_prepare_confounds_aroma(sample_confounds_timeseries, postprocessing_config, sample_melodic_mixing, sample_aroma_noise_ics,
     artifact_dir, helpers, request):
@@ -289,13 +301,14 @@ def test_prepare_confounds_aroma(sample_confounds_timeseries, postprocessing_con
 
     postprocessing_config["ProcessingSteps"] = ["AROMARegression", "TemporalFiltering", "IntensityNormalization"]
 
-    cf_workflow = build_confound_postprocessing_workflow(postprocessing_config, confound_file=sample_confounds_timeseries,
+    cf_workflow = build_confounds_processing_workflow(postprocessing_config, confound_file=sample_confounds_timeseries,
         out_file=out_path, mixing_file=sample_melodic_mixing, noise_file=sample_aroma_noise_ics,
         base_dir=test_path, crashdump_dir=test_path, tr=2)
 
     cf_workflow.run()
     
     assert True
+
 
 def test_postprocess_subject_job_setup(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
@@ -308,12 +321,8 @@ def test_postprocess_subject_job_setup(clpipe_fmriprep_dir, artifact_dir, helper
     log_dir.mkdir(parents=True, exist_ok=True)
     pybids_db_path = test_dir / "BIDS_index"
 
-    subject = PostProcessSubjectJob('1', bids_dir, fmriprep_dir, postproc_dir, config, pybids_db_path=pybids_db_path, log_dir=log_dir)
+    postprocess_subject('1', bids_dir, fmriprep_dir, postproc_dir, config, pybids_db_path=pybids_db_path, log_dir=log_dir)
 
-    subject.setup()
-
-    # Should be a task/run for each of rest, task1, task2-run1, task2-run2
-    assert len(subject.image_jobs) == 4
 
 def test_postprocess_subject_job(clpipe_fmriprep_dir, config_file, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
@@ -325,9 +334,8 @@ def test_postprocess_subject_job(clpipe_fmriprep_dir, config_file, artifact_dir,
     log_dir.mkdir(parents=True, exist_ok=True)
     pybids_db_path = test_dir / "BIDS_index"
 
-    subject = PostProcessSubjectJob('1', bids_dir, fmriprep_dir, postproc_dir, config_file, pybids_db_path=pybids_db_path, log_dir=log_dir)
+    postprocess_subject('1', bids_dir, fmriprep_dir, postproc_dir, config_file, pybids_db_path=pybids_db_path, log_dir=log_dir)
 
-    subject()
 
 def test_postprocess_subject_with_confounds(clpipe_fmriprep_dir, config_file_confounds, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
@@ -339,8 +347,8 @@ def test_postprocess_subject_with_confounds(clpipe_fmriprep_dir, config_file_con
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    subject = PostProcessSubjectJob('1', bids_dir, fmriprep_dir, postproc_dir, config_file_confounds, pybids_db_path=pybids_db_path, log_dir=log_dir)
-    subject()
+    postprocess_subject('1', bids_dir, fmriprep_dir, postproc_dir, config_file_confounds, pybids_db_path=pybids_db_path, log_dir=log_dir)
+
 
 def test_postprocess_subject_aroma(clpipe_fmriprep_dir, config_file_aroma, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
@@ -352,8 +360,8 @@ def test_postprocess_subject_aroma(clpipe_fmriprep_dir, config_file_aroma, artif
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    subject = PostProcessSubjectJob('1', bids_dir, fmriprep_dir, postproc_dir, config_file_aroma, pybids_db_path=pybids_db_path, log_dir=log_dir)
-    subject()
+    postprocess_subject('1', bids_dir, fmriprep_dir, postproc_dir, config_file_aroma, pybids_db_path=pybids_db_path, log_dir=log_dir)
+
 
 def test_postprocess_subject_aroma_with_confound_processing(clpipe_fmriprep_dir, config_file_aroma_confounds, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
@@ -365,8 +373,8 @@ def test_postprocess_subject_aroma_with_confound_processing(clpipe_fmriprep_dir,
     log_dir = Path(test_dir / "logs" / "postproc_logs")
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    subject = PostProcessSubjectJob('1', bids_dir, fmriprep_dir, postproc_dir, config_file_aroma_confounds, pybids_db_path=pybids_db_path, log_dir=log_dir)
-    subject()
+    postprocess_subject('1', bids_dir, fmriprep_dir, postproc_dir, config_file_aroma_confounds, pybids_db_path=pybids_db_path, log_dir=log_dir)
+
 
 def test_postprocess2_wf_fslmaths_temporal_filter(artifact_dir, postprocessing_config, request, sample_raw_image, sample_raw_image_mask, 
     sample_confounds_timeseries, plot_img, write_graph, helpers):
@@ -391,6 +399,7 @@ def test_postprocess2_wf_fslmaths_temporal_filter(artifact_dir, postprocessing_c
 
     assert True
 
+
 def test_postprocess2_wf_resample(artifact_dir, postprocessing_config, request, sample_raw_image, sample_reference, sample_raw_image_mask, plot_img, write_graph, helpers):
 
     postprocessing_config["ProcessingSteps"] = ["SpatialSmoothing", "IntensityNormalization", "TemporalFiltering", "Resample"]
@@ -411,5 +420,3 @@ def test_postprocess2_wf_resample(artifact_dir, postprocessing_config, request, 
         helpers.plot_4D_img_slice(out_path, "postProcessed.png")
 
     assert True
-
-
