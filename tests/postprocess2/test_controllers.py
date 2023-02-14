c9e75ffdb0aa40160c36971877a120b02aa04a39
Will Asciutto <wasciutto@unc.edu>
2023-02-14 13:25:02 -0500

    Remove postprocess2 test module and replace with test_controllers.py. Merge test_func.py into this module.

--- tests/postprocess2/test_controllers.py
+++ tests/postprocess2/test_controllers.py
@@ -8,6 +8,21 @@ from pathlib import Path
 def test_postprocess_subjects_dir(clpipe_fmriprep_dir, artifact_dir, helpers, request):
     fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
     config = clpipe_fmriprep_dir / "clpipe_config.json"
+    bids_dir = clpipe_fmriprep_dir / "data_BIDS"
+    test_dir = helpers.create_test_dir(artifact_dir, request.node.name)
+    postproc_dir = Path(test_dir / "data_postprocessed")
+    log_dir = Path(test_dir / "logs" / "postproc_logs")
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+
+    with pytest.raises(SystemExit):
+        postprocess_subjects(config_file=config, fmriprep_dir=fmriprep_dir, bids_dir=bids_dir,
+            output_dir=postproc_dir, log_dir=log_dir)
+
+
+def test_postprocess_subjects_dir(clpipe_fmriprep_dir, artifact_dir, helpers, request):
+    fmriprep_dir = clpipe_fmriprep_dir / "data_fmriprep" / "fmriprep"
+    config = clpipe_fmriprep_dir / "clpipe_config.json"
     glm_config = clpipe_fmriprep_dir / "glm_config.json"
     test_dir = helpers.create_test_dir(artifact_dir, request.node.name)
     postproc_dir = Path(test_dir / "data_postprocessed")
