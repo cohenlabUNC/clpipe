da3f3a4b64dbe471bd5e369fa840d0ed681d0879
Will Asciutto <wasciutto@unc.edu>
2023-02-20 12:31:18 -0500

    Add a short script to load modules needed for development.

---
+++ setup_modules.sh
@@ -0,0 +1,13 @@
+# Setup modules needed for the development environment
+
+# Clear any loaded modules
+module purge
+
+# Load all dependent modules
+module load fsl/6.0.3
+module load freesurfer/6.0.0
+module load afni/20.3.00
+module load r/4.2.1
+module load flywheel/16.4.0
+module load dcm2niix/1.0.20211006
+module load flywheel/16.4.0
