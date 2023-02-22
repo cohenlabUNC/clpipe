e76c9e8f20cf8f15de36cb653a65c80818bf9240
Will Asciutto <wasciutto@unc.edu>
2023-02-22 14:11:04 -0500

    Move the setup of GLM log directory before the config file dump, so that GLM log directory is filled out properly in user's newly setup GLM config file.

--- clpipe/config_json_parser.py
+++ clpipe/config_json_parser.py
@@ -126,10 +126,12 @@ class ClpipeConfigParser:
         os.mkdir(os.path.join(project_path, "l2_fsfs"))
         os.mkdir(os.path.join(project_path, "l2_gfeat_folders"))
 
-        glm_config.config_json_dump(project_path, "glm_config.json")
-        shutil.copyfile(resource_filename('clpipe', 'data/l2_sublist.csv'), os.path.join(project_path, "l2_sublist.csv"))
         glm_config.config['GLMSetupOptions']['LogDirectory'] = os.path.join(project_path, "logs", "glm_setup_logs")
         os.mkdir(os.path.join(project_path, "logs", "glm_setup_logs"))
+
+        glm_config.config_json_dump(project_path, "glm_config.json")
+        shutil.copyfile(resource_filename('clpipe', 'data/l2_sublist.csv'), os.path.join(project_path, "l2_sublist.csv"))
+        
 
     def setup_fmriprep_directories(self, bidsDir, workingDir, outputDir, log_dir = None):
         if bidsDir is not None:
