380267c14a6a293c771b31d7657aa26a31331ce1
Will Asciutto <wasciutto@unc.edu>
2023-02-22 13:06:01 -0500

    Rename glm_setup to match module.

--- tests/test_glm_setup.py
+++ tests/test_glm_setup.py
@@ -1,7 +1,8 @@
 import pytest
+from pathlib import Path
 from clpipe.glm_setup import glm_setup
 
-def test_glm_setup(config_file: Path, glm_config_file: Path) -> None:
+def test_glm_setup_basic(config_file: Path, glm_config_file: Path) -> None:
     """Check basic attempt to run fmriprep_process."""
 
     glm_setup(config_file=config_file, glm_config_file=glm_config_file)
