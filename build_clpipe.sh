501567e8a52fefca8d2c33c47811b15b2fafa637
Will Asciutto <wasciutto@unc.edu>
2023-02-27 18:21:59 -0500

    Add some messages to the build script.

--- build_clpipe.sh
+++ build_clpipe.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # A simple script to build clpipe and generate the documentation
+echo "Starting build"
 
 # Build the .whl file
+echo "Building wheel"
 python -m build --wheel
 
 # Build the documentation
+echo "Building Documentation"
 (cd docs; make html)
 
 # Build bash auto-completion script
+echo "Building auto-completion bash script"
 source generate_autocomplete
+
+echo "Build complete"
