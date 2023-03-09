61fc62ec928384ef29cd836076033763f88fa8e8
Will Asciutto <wasciutto@unc.edu>
2023-03-09 12:43:01 -0500

    Add deprecation test for glm_setup.

--- tests/test_glm_setup.py
+++ tests/test_glm_setup.py
@@ -7,3 +7,10 @@ def test_glm_setup_basic(config_file: Path, glm_config_file: Path):
 
     glm_setup(config_file=config_file, glm_config_file=glm_config_file)
     assert True
+
+
+def test_glm_setup_deprecation_warning(config_file: Path, glm_config_file: Path):
+    """Check basic attempt to run fmriprep_process."""
+
+    with pytest.raises(DeprecationWarning):
+        glm_setup(config_file=config_file, glm_config_file=glm_config_file)
