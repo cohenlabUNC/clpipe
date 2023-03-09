438e383eaadb7839317e0f6707e4ca7fb39a7497
Will Asciutto <wasciutto@unc.edu>
2023-03-09 12:43:51 -0500

    Adjust backwards compatible glm setup test to use fixtures.

--- tests/test_glm_setup.py
+++ tests/test_glm_setup.py
@@ -1,7 +1,7 @@
 import pytest
 from pathlib import Path
 from clpipe.glm_setup import glm_setup
-import json
+
 
 def test_glm_setup_basic(config_file: Path, glm_config_file: Path):
     """ Check basic attempt to run glm_setup.
@@ -14,11 +14,12 @@ def test_glm_setup_basic(config_file: Path, glm_config_file: Path):
     assert excinfo.value.code == 1
 
 
-def test_glm_setup_backwards_compatible():
+def test_glm_setup_backwards_compatible(clpipe_dir_old_glm_config: Path):
     """ Ensure glm_setup still works with the old glm config settings and that
         it raises a deprecation warning.
     """
+    config_file = clpipe_dir_old_glm_config / "clpipe_config.json"
+    glm_config_file = clpipe_dir_old_glm_config / "glm_config.json"
+
     with pytest.warns():
-        with open("tests/data/old_GLMConfig.json", 'r') as old_glm_config_file:
-            old_glm_config = json.load(old_glm_config_file)
-    assert False
+        glm_setup(config_file=config_file, glm_config_file=glm_config_file)
