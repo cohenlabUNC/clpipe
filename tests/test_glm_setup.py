ac9df72b802d8fc7e83c20f77be5a49ca90e8839
Will Asciutto <wasciutto@unc.edu>
2023-03-09 12:43:02 -0500

    Add test for using old glm.

--- tests/test_glm_setup.py
+++ tests/test_glm_setup.py
@@ -1,6 +1,7 @@
 import pytest
 from pathlib import Path
 from clpipe.glm_setup import glm_setup
+import json
 
 def test_glm_setup_basic(config_file: Path, glm_config_file: Path):
     """Check basic attempt to run fmriprep_process."""
@@ -14,3 +15,8 @@ def test_glm_setup_deprecation_warning(config_file: Path, glm_config_file: Path)
 
     with pytest.warns():
         glm_setup(config_file=config_file, glm_config_file=glm_config_file)
+
+
+def test_glm_setup_backwards_compatible(config_file: Path):
+    with open("data/old_GLMConfig.json", 'r') as old_glm_config_file:
+        old_glm_config = json.load(old_glm_config_file)
