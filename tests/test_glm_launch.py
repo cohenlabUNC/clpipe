fa27ed9831eff238ae6f971f621edb2eedc8eb21
Will Asciutto <wasciutto@unc.edu>
2023-03-09 19:09:54 -0500

    Improve tests to cover error codes and other levels.

--- tests/test_glm_launch.py
+++ tests/test_glm_launch.py
@@ -2,7 +2,28 @@ import pytest
 from pathlib import Path
 from clpipe.glm_launch import glm_launch
 
-def test_glm_launch_controller(glm_config_file: Path):
+def test_glm_launch_controller_L1(glm_config_file: Path):
     """Check running glm_launch controller."""
 
-    glm_launch(glm_config_file=glm_config_file, level="L1", model="example_L1")
+    with pytest.raises(SystemExit) as e:
+        glm_launch(glm_config_file=glm_config_file, level="L1", model="example_L1")
+
+    assert e.value.code == 0
+
+
+def test_glm_launch_controller_L2(glm_config_file: Path):
+    """Check running glm_launch controller."""
+
+    with pytest.raises(SystemExit) as e:
+        glm_launch(glm_config_file=glm_config_file, level="L2", model="tworun_gngreg")
+
+    assert e.value.code == 0
+
+
+def test_glm_launch_controller_invalid_level(glm_config_file: Path):
+    """Check that launch controller rejects an invalid level."""
+
+    with pytest.raises(SystemExit) as e:
+        glm_launch(glm_config_file=glm_config_file, level="L4", model="example_L1")
+
+    assert e.value.code == 1
