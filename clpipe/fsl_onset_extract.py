137b87d20c4d19918544cf4e3341b127500bdc38
Will Asciutto <wasciutto@unc.edu>
2023-04-18 12:48:38 -0400

    Use new logging system.

--- clpipe/fsl_onset_extract.py
+++ clpipe/fsl_onset_extract.py
@@ -1,26 +1,24 @@
 import os
 import glob
-import logging
-import sys
 import numpy
 import pandas
 import numpy as np
 
-from .error_handler import exception_handler
 from .config_json_parser import ClpipeConfigParser, GLMConfigParser
+from .utils import get_logger
 
+STEP_NAME = "fsl_onset_extract"
 
 def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
-    if not debug:
-        sys.excepthook = exception_handler
-        logging.basicConfig(level=logging.INFO)
-    else:
-        logging.basicConfig(level=logging.DEBUG)
+    
 
     config = ClpipeConfigParser()
     config.config_updater(config_file)
 
     glm_config = GLMConfigParser(glm_config_file)
+
+    project_dir = config.config["ProjectDirectory"]
+    logger = get_logger(STEP_NAME, debug=debug, f_name=os.path.join(project_dir, "logs"))
 
     search_string = os.path.abspath(
         os.path.join(config.config["FMRIPrepOptions"]['BIDSDirectory'], "**",
@@ -32,11 +30,11 @@ def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
 
     for file in files:
         data = pandas.read_table(file)
-        logging.info("Processing " + os.path.basename(file))
+        logger.info("Processing " + os.path.basename(file))
         for trial_type in target_trials:
             temp = data.loc[data[glm_config.config["Level1Onsets"]["TrialTypeVariable"]] == trial_type]
             if len(temp) == 0:
-                logging.warning("Trial Type: "+ trial_type + ", has no entries. If this is unexpected, check your GLM config file for misspellings/incorrect entries in the ['Level1Onsets']['TrialTypeToExtract']")
+                logger.warning("Trial Type: "+ trial_type + ", has no entries. If this is unexpected, check your GLM config file for misspellings/incorrect entries in the ['Level1Onsets']['TrialTypeToExtract']")
             else:
                 onsets = temp['onset']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
                 duration = temp['duration']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
@@ -47,6 +45,6 @@ def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
                 to_file = np.array([onsets, duration, p_response])
                 file_name = os.path.basename(file).replace(glm_config.config["Level1Onsets"]['EventFileSuffix'], trial_type + ".txt")
                 output = os.path.join(glm_config.config["Level1Onsets"]['EVDirectory'], file_name)
-                logging.debug(output)
+                logger.debug(output)
                 numpy.savetxt(output, np.transpose(to_file), "%8.4f")
 
