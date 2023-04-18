549a3ac89f80bd050a010643e9bacd8e8729de81
Will Asciutto <wasciutto@unc.edu>
2023-04-18 13:07:16 -0400

    Expand logger messages, tidy up code, add a case to exit and print error if no files are found.

--- clpipe/fsl_onset_extract.py
+++ clpipe/fsl_onset_extract.py
@@ -3,6 +3,7 @@ import glob
 import numpy
 import pandas
 import numpy as np
+import sys
 
 from .config_json_parser import ClpipeConfigParser, GLMConfigParser
 from .utils import get_logger
@@ -10,9 +11,7 @@ from .utils import get_logger
 STEP_NAME = "fsl_onset_extract"
 DEPRECATION_MSG = "WARNING: Using deprecated GLM setup file."
 
-def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
-    
-
+def fsl_onset_extract(config_file=None, glm_config_file=None, debug=False):
     config = ClpipeConfigParser()
     config.config_updater(config_file)
 
@@ -35,20 +34,41 @@ def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
         logger.warn(DEPRECATION_MSG)
 
     search_string = os.path.abspath(
-        os.path.join(config.config["FMRIPrepOptions"]['BIDSDirectory'], "**",
-                     "*" +"task-"+ task_name+"*"+ glm_config.config["Level1Onsets"]['EventFileSuffix']))
+        os.path.join(
+            config.config["FMRIPrepOptions"]['BIDSDirectory'], "**",
+            "*" + "task-" + task_name + "*" +
+            glm_config.config["Level1Onsets"]['EventFileSuffix']
+        )
+    )
+    logger.debug(search_string)
 
     files = glob.glob(search_string, recursive=True)
 
     target_trials = glm_config.config["Level1Onsets"]['TrialTypeToExtract']
+    logger.debug(
+        f"EV files will be saved to: {glm_config.config['Level1Onsets']['EVDirectory']}"
+    )
+    logger.info(f"Targeting trials: {target_trials}")
 
+    if len(files) == 0:
+        logger.error("No files found for search string: {search_string}")
+        sys.exit(1)
+
+    logger.info(f"Starting onset extraction.")
     for file in files:
         data = pandas.read_table(file)
         logger.info("Processing " + os.path.basename(file))
         for trial_type in target_trials:
-            temp = data.loc[data[glm_config.config["Level1Onsets"]["TrialTypeVariable"]] == trial_type]
+            temp = data.loc[data[
+                glm_config.config["Level1Onsets"]["TrialTypeVariable"]] == trial_type
+            ]
             if len(temp) == 0:
-                logger.warning("Trial Type: "+ trial_type + ", has no entries. If this is unexpected, check your GLM config file for misspellings/incorrect entries in the ['Level1Onsets']['TrialTypeToExtract']")
+                logger.warning(
+                    "Trial Type: " + trial_type + ", has no entries."
+                    "If this is unexpected, check your GLM config file "
+                    "for misspellings/incorrect entries in the "
+                    "['Level1Onsets']['TrialTypeToExtract']"
+                )
             else:
                 onsets = temp['onset']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
                 duration = temp['duration']/glm_config.config["Level1Onsets"]["TimeConversionFactor"]
@@ -57,8 +77,13 @@ def fsl_onset_extract(config_file=None, glm_config_file = None, debug = None):
                     p_response = temp[glm_config.config["Level1Onsets"]["ParametricResponseVariable"]]
 
                 to_file = np.array([onsets, duration, p_response])
-                file_name = os.path.basename(file).replace(glm_config.config["Level1Onsets"]['EventFileSuffix'], trial_type + ".txt")
-                output = os.path.join(glm_config.config["Level1Onsets"]['EVDirectory'], file_name)
+                file_name = os.path.basename(file).replace(
+                    glm_config.config["Level1Onsets"]['EventFileSuffix'],
+                    trial_type + ".txt"
+                )
+                output = os.path.join(
+                    glm_config.config["Level1Onsets"]['EVDirectory'],
+                    file_name
+                )
                 logger.debug(output)
-                #numpy.savetxt(output, np.transpose(to_file), "%8.4f")
-
+                numpy.savetxt(output, np.transpose(to_file), "%8.4f")
