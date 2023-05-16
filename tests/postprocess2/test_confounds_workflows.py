1a232b2f4210ef06adc74e1316557e6716a5ac4b
Will Asciutto <wasciutto@unc.edu>
2023-05-16 16:17:34 -0400

    Remove unnecessary test.

--- tests/postprocess2/test_confounds_workflows.py
+++ tests/postprocess2/test_confounds_workflows.py
@@ -12,7 +12,7 @@ def test_build_confounds_processing_workflow_2_steps(
     request,
 ):
     """Check that confounds processing works with IN and TF."""
-    
+
     postprocessing_config["ProcessingSteps"] = [
         "IntensityNormalization",
         "TemporalFiltering",
@@ -119,23 +119,6 @@ def test_build_confounds_prep_workflow_no_scrubs(
 
     wf.write_graph(dotfilename=test_path / "confounds_flow", graph2use="colored")
 
-    wf.run()
-
-
-def test_build_get_scrub_targets_workflow(
-    artifact_dir, sample_confounds_timeseries, request, helpers
-):
-    """Test getting scrub targets from confounds given an FD threshold."""
-
-    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
-    scrubbed_path = test_path / "scrubbed_confounds.tsv"
-
-    wf = build_scrubbing_workflow(
-        in_file=sample_confounds_timeseries,
-        out_file=scrubbed_path,
-        base_dir=test_path,
-        crashdump_dir=test_path,
-    )
     wf.run()
 
 
