74821b3fb2ed29485575cc349b58a84be77ad441
Will Asciutto <wasciutto@unc.edu>
2023-05-16 16:17:34 -0400

    Add test for confounds w/ aroma.

--- tests/postprocess2/test_confounds_workflows.py
+++ tests/postprocess2/test_confounds_workflows.py
@@ -36,6 +36,40 @@ def test_build_confounds_processing_workflow_2_steps(
     wf.run()
 
 
+def test_build_confounds_processing_workflow_aroma_regression(
+    artifact_dir,
+    postprocessing_config,
+    sample_confounds_timeseries,
+    sample_melodic_mixing,
+    sample_aroma_noise_ics,
+    helpers,
+    request,
+):
+    """Check that confounds processing works with IN and TF."""
+
+    postprocessing_config["ProcessingSteps"] = [
+        "AROMARegression"
+    ]
+
+    test_path = helpers.create_test_dir(artifact_dir, request.node.name)
+    out_path = test_path / "postprocessed.tsv"
+
+    wf = build_confounds_processing_workflow(
+        postprocessing_config,
+        confounds_file=sample_confounds_timeseries,
+        export_file=out_path,
+        tr=2,
+        mixing_file=sample_melodic_mixing,
+        noise_file=sample_aroma_noise_ics,
+        base_dir=test_path,
+        crashdump_dir=test_path,
+    )
+
+    wf.write_graph(dotfilename=test_path / "confounds_flow", graph2use="colored")
+
+    wf.run()
+
+
 def test_build_confounds_processing_workflow_2_steps_no_scrub(
     artifact_dir,
     postprocessing_config,
