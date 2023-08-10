0118bdb1cc39f4acd99c03cdd190608883ed307e
Will Asciutto <wasciutto@unc.edu>
2023-08-10 12:34:05 -0400

    Update version.

--- clpipe/config/package.py
+++ clpipe/config/package.py
@@ -1,5 +1,5 @@
 PACKAGE_NAME = 'clpipe'
-VERSION = '1.8.1'
+VERSION = '1.8.1.1'
 
 DESCRIPTION = 'clpipe: MRI processing pipeline for high performance clusters'
 REPO_URL = 'https://github.com/cohenlabUNC/clpipe'
