58fb7da699e5310b611f17bd07c9786a8056f3c0
Will Asciutto <wasciutto@unc.edu>
2023-09-12 06:18:06 -0400

    Move GLMConfigParser to config and rename/restructure to GLMOptions to match main pipeline options config.

--- clpipe/config/glm.py
+++ clpipe/config/glm.py
@@ -1,5 +1,70 @@
+import os
+import json
+
+DEFAULT_CONFIG_FILE_NAME = "defaultGLMConfig.json"
+
 # GLM Levels
 VALID_L1 = ["1", "L1", "l1", "level1"]
 VALID_L2 = ["2", "L2", "l2", "level2"]
 L1 = VALID_L1[1]
 L2 = VALID_L2[1]
+
+class GLMOptions:
+    """Configuration related to the GLM modules
+    
+    Config values are found in this object's 'attribute' as a dictionary matching
+        the json structure of the default GLM config file.
+    """
+
+    def __init__(self, glm_config_file: os.PathLike = None):
+        """
+        Args:
+            glm_config_file (os.PathLike, optional): Path to GLM config file. 
+            May be relative. Defaults to None.
+        """
+        # Use the default GLM config file if none provided
+        if glm_config_file is None:
+            from pkg_resources import resource_stream
+
+            with resource_stream(__name__, f'data/{DEFAULT_CONFIG_FILE_NAME}') as def_config:
+                self.config = json.load(def_config)
+        else:
+            with open(os.path.abspath(glm_config_file), "r") as config_file:
+                self.config = json.load(config_file)
+
+
+    def config_json_dump(self, outputdir: os.PathLike, file_name: os.PathLike) -> os.PathLike:
+        """Dump the state of this configuration to json.
+
+        Args:
+            outputdir (os.PathLike): Output destination. May be relative.
+            file_name (os.PathLike): Name of your GLM config file.
+        """
+        file_name = DEFAULT_CONFIG_FILE_NAME if file_name is None else None
+        outpath = os.path.join(os.path.abspath(outputdir), file_name)
+
+        with open(outpath, 'w') as fp:
+            json.dump(self.config, fp, indent="\t")
+        
+        return outpath
+    
+    def populate_project_paths(self, project_directory: os.PathLike) -> None:
+        """Generate default project paths given a specific project root.
+
+        Args:
+            project_directory (os.PathLike): Path to the root project directory.
+        """
+        import os
+
+        self.config['ParentClpipeConfig'] = os.path.join(project_directory, "clpipe_config.json")
+
+        self.config['Level1Setups'][0]['TargetDirectory'] = os.path.join(project_directory, "data_postproc2", "default")
+        self.config['Level1Setups'][0]['FSFDir'] = os.path.join(project_directory, "l1_fsfs")
+        self.config['Level1Setups'][0]['EVDirectory'] = os.path.join(project_directory, "data_onsets")
+        self.config['Level1Setups'][0]['ConfoundDirectory'] = os.path.join(project_directory, "data_postproc2", "default")
+        self.config['Level1Setups'][0]['OutputDir'] = os.path.join(project_directory, "l1_feat_folders")
+        self.config['Level1Setups'][0]['LogDir'] = os.path.join(project_directory, "logs", "glm_logs", "L1_launch")
+
+        self.config['Level2Setups'][0]['OutputDir'] = os.path.join(project_directory, "l2_gfeat_folders")
+        self.config['Level2Setups'][0]['FSFDir'] = os.path.join(project_directory, "l2_fsfs")
+        self.config['Level2Setups'][0]['LogDir'] = os.path.join(project_directory, "logs", "glm_logs", "L2_launch")
