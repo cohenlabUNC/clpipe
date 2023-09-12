6f2b0fbb99b4e7080c094a54e41b1c23adb05593
Will Asciutto <wasciutto@unc.edu>
2023-09-12 06:18:07 -0400

    Update tests to use new configuration.

--- tests/test_roi_extractor.py
+++ tests/test_roi_extractor.py
@@ -1,4 +1,5 @@
 import pytest
+from clpipe.config.options import ProjectOptions
 from pathlib import Path
 
 from clpipe.roi_extractor import (
@@ -18,13 +19,10 @@ def test_fmri_roi_extraction(config_file_postproc):
 def test_fmri_roi_extraction_overlap_ok(clpipe_postproc_dir):
     config_file_path = clpipe_postproc_dir / "clpipe_config.json"
 
-    # Replace once new config is implemented
-    from clpipe.config_json_parser import ClpipeConfigParser
-    configParser = ClpipeConfigParser()
-    configParser.config_updater(config_file_path)
+    config: ProjectOptions = ProjectOptions.load(config_file_path)
 
-    configParser.config["ROIExtractionOptions"]["OverlapOk"] = True
-    configParser.config_json_dump(clpipe_postproc_dir, "clpipe_config.json")
+    config.roi_extraction.overlap_ok = True
+    config.dump(config_file_path)
 
     """Basic test of roi_extraction using single run."""
     fmri_roi_extraction(
@@ -43,7 +41,7 @@ def test_fmri_roi_extraction_legacy(config_file_postproc_legacy_fmriprep):
     )
 
 def test_fmri_roi_extract_image(clpipe_postproc_dir, artifact_dir, request, helpers):
-    """Test ROI extraction on a single subject - show output."""
+    """Given an fmriprep target image, test ROI extraction on a single subject - show output."""
     artifact_dir = helpers.create_test_dir(artifact_dir, request.node.name)
 
     logger = get_logger(STEP_NAME, debug=True, log_dir=clpipe_postproc_dir / "logs")
@@ -55,51 +53,14 @@ def test_fmri_roi_extract_image(clpipe_postproc_dir, artifact_dir, request, help
     config_file_path = clpipe_postproc_dir / "clpipe_config.json"
 
     # Replace once new config is implemented
-    from clpipe.config_json_parser import ClpipeConfigParser
-    configParser = ClpipeConfigParser()
-    configParser.config_updater(config_file_path)
-
-    configParser.config["ROIExtractionOptions"]["OutputDirectory"] = artifact_dir
+    config: ProjectOptions = ProjectOptions.load(config_file_path)
+    config.roi_extraction.output_directory = artifact_dir
 
     Path(artifact_dir / "bigbrain").mkdir(exist_ok=True)
 
     fmri_roi_extract_image(
         str(image_path),
-        configParser,
-        "bigbrain",
-        "clpipe/data/atlases/bigbrain/BigBrain300_MNI_coordinates.txt",
-        "sphere",
-        5,
-        True,
-        True,
-        logger,
-    )
-
-def test_fmri_roi_extract_image_fmriprep(clpipe_fmriprep_dir, artifact_dir, request, helpers):
-    """Given an fmriprep target image, test ROI extraction on a single subject - show output."""
-    artifact_dir = helpers.create_test_dir(artifact_dir, request.node.name)
-
-    logger = get_logger(STEP_NAME, debug=True, log_dir=clpipe_fmriprep_dir / "logs")
-    image_name = "sub-1_task-gonogo_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
-    image_path = (
-        clpipe_fmriprep_dir
-        / "data_fmriprep/sub-1/func" / image_name
-    )
-    config_file_path = clpipe_fmriprep_dir / "clpipe_config.json"
-
-    # Replace once new config is implemented
-    from clpipe.config_json_parser import ClpipeConfigParser
-    configParser = ClpipeConfigParser()
-    configParser.config_updater(config_file_path)
-
-    configParser.config["ROIExtractionOptions"]["OutputDirectory"] = artifact_dir
-    configParser.config["ROIExtractionOptions"]["TargetSuffix"] = "desc-preproc_bold.nii.gz"
-
-    Path(artifact_dir / "bigbrain").mkdir(exist_ok=True)
-
-    fmri_roi_extract_image(
-        str(image_path),
-        configParser,
+        config,
         "bigbrain",
         "clpipe/data/atlases/bigbrain/BigBrain300_MNI_coordinates.txt",
         "sphere",
