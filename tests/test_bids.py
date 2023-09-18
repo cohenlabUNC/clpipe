a5837448039e7e0397386ab860c1d6c08581f88d
yuvrajjain2003 <yjain@unc.edu>
2023-09-18 14:29:23 -0400

    Add comments to describe functionality of test_get_bids

--- tests/test_bids.py
+++ tests/test_bids.py
@@ -4,5 +4,9 @@ from pathlib import Path
 from clpipe.bids import get_bids
 
 def test_get_bids(clpipe_bids_dir: Path):
-    get_bids(clpipe_bids_dir)
+    layout = get_bids(clpipe_bids_dir / "data_BIDS")
+    
+    # Practice stuff querying for layout...
+    # Test for values returned from the queries.
+
     
