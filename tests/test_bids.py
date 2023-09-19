e24668b24d2c9e2b213b8a80ddfad8fed7736211
yuvrajjain2003 <yjain@unc.edu>
2023-09-19 00:47:46 -0400

    Add regex for indexer.

--- tests/test_bids.py
+++ tests/test_bids.py
@@ -19,17 +19,18 @@ def test_get_bids(clpipe_fmriprep_dir):
     assert index_sub_0_path == actual_sub_0_path
 
 
-def test_get_bids_derivatives(clpipe_fmriprep_dir):
+def test_ignore_BIDS_Indexer(clpipe_fmriprep_dir):
     project_dir = clpipe_fmriprep_dir
-    layout = get_bids(
+    pattern = [re.compile(r".*\.html$")]
+    indexer: BIDSLayoutIndexer = BIDSLayoutIndexer(ignore=pattern, validate=False)
+    layout: BIDSLayout = get_bids(
         project_dir / "data_BIDS",
         database_path=project_dir / "BIDS_index",
         index_metadata=True,
         fmriprep_dir=project_dir / "data_fmriprep",
+        refresh=True,
     )
 
-    index_sub_0_path = layout.get(subject="0", return_type="filename")[0]
-    actual_sub_0_path = str(
-        clpipe_fmriprep_dir / "data_BIDS/sub-0/func/sub-0_task-rest_bold.nii.gz"
-    )
-    assert index_sub_0_path == actual_sub_0_path
+    indexer(layout)
+    # temp = layout.get()
+    assert True
