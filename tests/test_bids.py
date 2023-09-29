cb3f9bcacf4ec21ff2cabd915f71893fad62319e
yuvrajjain2003 <yjain@unc.edu>
2023-09-29 14:12:53 -0400

    Update test to include nii.gz pattern.

--- tests/test_bids.py
+++ tests/test_bids.py
@@ -22,8 +22,9 @@ def test_get_bids(clpipe_fmriprep_dir):
 
 def test_ignore_BIDS_Indexer(clpipe_fmriprep_dir):
     project_dir = clpipe_fmriprep_dir
-    pattern = [re.compile(r".*\.html$")]
-    # layout: BIDSLayout = get_bids(
+    pattern_nii_gz = [re.compile(r".*\.nii.gz$")]
+    pattern_html = [re.compile(r".*\.tsv$")]
+    #  layout: BIDSLayout = get_bids(
     #     project_dir / "data_BIDS",
     #     database_path=project_dir / "BIDS_index",
     #     index_metadata=True,
@@ -32,7 +33,13 @@ def test_ignore_BIDS_Indexer(clpipe_fmriprep_dir):
     #     refresh=True,
     # )
     # temp = layout.get()
-    indexer: BIDSLayoutIndexer = BIDSLayoutIndexer(ignore=pattern)
-    layout: BIDSLayout = BIDSLayout(project_dir / "data_BIDS", indexer=indexer)
-    temp = layout.get()
+    indexer: BIDSLayoutIndexer = BIDSLayoutIndexer(ignore=pattern_nii_gz)
+    layout: BIDSLayout = BIDSLayout(
+        project_dir / "data_BIDS",
+        database_path=project_dir / "BIDS_index",
+        indexer=indexer,
+        derivatives=project_dir / "data_fmriprep",
+        ignore=pattern_nii_gz,
+    )
+
     assert True
