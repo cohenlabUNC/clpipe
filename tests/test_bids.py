479326351c263f52c85f867fdcb97823dbbb2c03
yuvrajjain2003 <yjain@unc.edu>
2023-09-29 01:19:46 -0400

    Possibly working indexer with ignore.

--- tests/test_bids.py
+++ tests/test_bids.py
@@ -23,13 +23,16 @@ def test_get_bids(clpipe_fmriprep_dir):
 def test_ignore_BIDS_Indexer(clpipe_fmriprep_dir):
     project_dir = clpipe_fmriprep_dir
     pattern = [re.compile(r".*\.html$")]
-    layout: BIDSLayout = get_bids(
-        project_dir / "data_BIDS",
-        database_path=project_dir / "BIDS_index",
-        index_metadata=True,
-        #fmriprep_dir=project_dir / "data_fmriprep",
-        #ignore=pattern,
-        refresh=True,
-    )
+    # layout: BIDSLayout = get_bids(
+    #     project_dir / "data_BIDS",
+    #     database_path=project_dir / "BIDS_index",
+    #     index_metadata=True,
+    #     #fmriprep_dir=project_dir / "data_fmriprep",
+    #     #ignore=pattern,
+    #     refresh=True,
+    # )
+    # temp = layout.get()
+    indexer: BIDSLayoutIndexer = BIDSLayoutIndexer(ignore=pattern)
+    layout: BIDSLayout = BIDSLayout(project_dir / "data_BIDS", indexer=indexer)
     temp = layout.get()
     assert True
