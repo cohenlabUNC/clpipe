7ba054324a11489a241f460cfa617224e552d7c8
yuvrajjain2003 <yjain@unc.edu>
2023-09-28 21:11:05 -0400

    Update ParallelManagerConfig to work with default batchConfig jsons.

--- clpipe/config/parallel.py
+++ clpipe/config/parallel.py
@@ -3,19 +3,60 @@ from dataclasses import dataclass, field
 from pathlib import Path
 
 
-@dataclass
-class ParallelConfig:
-    """Configuration for ParallelManager."""
+# Questions:
 
-    output_directory: str = field(
-        default=".", metadata={"description": "Output directory for batch jobs."}
-    )
-    debug: bool = field(default=False, metadata={"description": "Enable debug mode."})
-    mem_use: str = field(
-        default=None, metadata={"description": "Memory usage configuration."}
-    )
-    time: str = field(default=None, metadata={"description": "Time configuration."})
-    threads: int = field(default=None, metadata={"description": "Number of threads."})
-    email: str = field(
-        default=None, metadata={"description": "Email address for job notifications."}
-    )
+# Frozen dataclasses
+# Do I need to add string literals after every attribute created here?
+
+
+@dataclass
+class ParralelManagerConfig:
+    """
+    Config dataclass for ParallelManager
+    """
+
+    SubmissionHead: str
+    SubmissionOptions: list = [{"command": "-n", "args": "1"}]
+    SubOptionsEqual: list = []
+    NThreadsCommand: str = "--cpus-per-task={nthreads}"
+    NThreads: str = "1"
+    MemoryCommand: str = "--mem={mem}"
+    MemoryDefault: str = "1000"
+    TimeCommand: str = "--time={time}"
+    TimeDefault: str = "1:0:0"
+    JobIDCommand: str = '--job-name="{jobid}"'
+    OutputCommand: str = "--output={output}"
+    CommandWrapper: str = '--wrap="{cmdwrap}"'
+    EmailAddress: str = ""
+    EmailCommand: str = "--mail-user {email} --mail-type=FAIL"
+    FMRIPrepBatchCommands: str = "-e --no-home"
+    NoQuotes: bool = False
+    TimeCommandActive: bool = True
+    ThreadCommandActive: bool = True
+    JobIDCommandActive: bool = True
+    OutputCommandActive: bool = True
+    SingularityBindPaths: str = "/proj,/pine,/work,/nas02,/nas"
+
+    # This is the best way I could figure out to use one dataclass but use different
+    # default configs.
+    # This is example config
+    # unc_config = BatchConfig.from_default("unc")
+    @classmethod
+    def from_default(cls, config_type="unc"):
+        defaults = {
+            "unc": {
+                "SubmissionHead": "sbatch --no-requeue",
+                "SubmissionOptions": [{"command": "-n", "args": "1"}],
+                "SingularityBindPaths": "/proj,/pine,/work,/nas02,/nas",
+            },
+            "pitt": {
+                "SubmissionHead": "bq",
+                "SubmissionOptions": [],
+                "TimeCommandActive": False,
+                "ThreadCommandActive": False,
+                "JobIDCommandActive": False,
+                "OutputCommandActive": False,
+                "SingularityBindPaths": "/proj,/pine,/nas02,/nas",
+            },
+        }
+        return cls(**defaults.get(config_type, {}))
