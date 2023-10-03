e07901f096d480cb4e0d5e932f17dfba96e74a60
yuvrajjain2003 <yjain@unc.edu>
2023-10-03 16:14:56 -0400

    Update config file to be snake_case, add field values for default to be UNC.

--- clpipe/config/parallel.py
+++ clpipe/config/parallel.py
@@ -12,56 +12,56 @@ class ParallelManagerConfig(Option):
     Config dataclass for ParallelManager
     """
 
-    SubmissionHead: str
-    SubmissionOptions: list = [{"command": "-n", "args": "1"}]
-    SubOptionsEqual: list = []
-    NThreadsCommand: str = "--cpus-per-task={nthreads}"
-    NThreads: str = "1"
-    MemoryCommand: str = "--mem={mem}"
-    MemoryDefault: str = "1000"
-    TimeCommand: str = "--time={time}"
-    TimeDefault: str = "1:0:0"
-    JobIDCommand: str = '--job-name="{jobid}"'
-    OutputCommand: str = "--output={output}"
-    CommandWrapper: str = '--wrap="{cmdwrap}"'
-    EmailAddress: str = ""
-    EmailCommand: str = "--mail-user {email} --mail-type=FAIL"
-    FMRIPrepBatchCommands: str = "-e --no-home"
-    NoQuotes: bool = False
-    TimeCommandActive: bool = True
-    ThreadCommandActive: bool = True
-    JobIDCommandActive: bool = True
-    OutputCommandActive: bool = True
-    SingularityBindPaths: str = "/proj,/pine,/work,/nas02,/nas"
+    submission_head = field(default="sbatch --no-requeue", metadata={"required": True})
+    submission_options = field(
+        default=[{"command": "-n", "args": "1"}], metadata={"required": True}
+    )
+    sub_options_equal = field(default=[], metadata={"required": True})
+    n_threads_command = field(
+        default="--cpus-per-task={nthreads}", metadata={"required": True}
+    )
+    n_threads = field(default="1", metadata={"required": True})
+    memory_command = field(default="--mem={mem}", metadata={"required": True})
+    memory_default = field(default="1000", metadata={"required": True})
+    time_command = field(default="--time={time}", metadata={"required": True})
+    time_default = field(default="1:0:0", metadata={"required": True})
+    job_id_command = field(default='--job-name="{jobid}"', metadata={"required": True})
+    output_command = field(default="--output={output}", metadata={"required": True})
+    command_wrapper = field(default='--wrap="{cmdwrap}"', metadata={"required": True})
+    email_address = field(default="", metadata={"required": True})
+    email_command = field(
+        default="--mail-user {email} --mail-type=FAIL", metadata={"required": True}
+    )
+    fmri_prep_batch_commands = field(
+        default="-e --no-home", metadata={"required": True}
+    )
+    no_quotes = field(default=False, metadata={"required": True})
+    time_command_active = field(default=True, metadata={"required": True})
+    thread_command_active = field(default=True, metadata={"required": True})
+    job_id_command_active = field(default=True, metadata={"required": True})
+    output_command_Active = field(default=True, metadata={"required": True})
+    singularity_bind_paths = field(
+        default="/proj,/pine,/work,/nas02,/nas", metadata={"required": True}
+    )
 
-    # This is the best way I could figure out to use one dataclass but use different
-    # default configs.
-    # This is example config
-    # unc_config = ParallelManagerConfig.from_default("unc")
     @classmethod
     def from_default(cls, config_type="unc"):
         defaults = {
             "unc": {
-                "SubmissionHead": "sbatch --no-requeue",
-                "SubmissionOptions": [{"command": "-n", "args": "1"}],
-                "SingularityBindPaths": "/proj,/pine,/work,/nas02,/nas",
+                "submission_head": field(default="sbatch --no-requeue"),
+                "submission_options": field(default=[{"command": "-n", "args": "1"}]),
+                "singularity_bind_paths": field(
+                    default="/proj,/pine,/work,/nas02,/nas"
+                ),
             },
             "pitt": {
-                "SubmissionHead": "bq",
-                "SubmissionOptions": [],
-                "TimeCommandActive": False,
-                "ThreadCommandActive": False,
-                "JobIDCommandActive": False,
-                "OutputCommandActive": False,
-                "SingularityBindPaths": "/proj,/pine,/nas02,/nas",
+                "submission_head": field(default="bq"),
+                "submission_options": field(default=[]),
+                "time_command_active": field(default=False),
+                "thread_command_active": field(default=False),
+                "job_id_command_active": field(default=False),
+                "output_command_active": field(default=False),
+                "singularity_bind_paths": field(default="/proj,/pine,/nas02,/nas"),
             },
         }
         return cls(**defaults.get(config_type, {}))
-
-    @classmethod
-    def from_json(cls, json_file_path):
-        with open(json_file_path, "r") as file:
-            config_data = json.load(file)
-        return cls(**config_data)
-
-    # USE THE OPTIONS LOAD.
