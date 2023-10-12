0c6e92e86beb9855bbf68b9f1ee18c5759094ca0
Will Asciutto <wasciutto@unc.edu>
2023-10-12 14:30:34 -0400

    Remove pine from bind path.

--- clpipe/config/package.py
+++ clpipe/config/package.py
@@ -1,5 +1,5 @@
 PACKAGE_NAME = 'clpipe'
-VERSION = '1.9.0.4'
+VERSION = '1.9.0.5'
 
 DESCRIPTION = 'clpipe: MRI processing pipeline for high performance clusters'
 REPO_URL = 'https://github.com/cohenlabUNC/clpipe'
