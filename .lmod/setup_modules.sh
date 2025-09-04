3bf070868ff4e11df2fbea57d2b658424deae2be
Dan Shallal <shallald@unc.edu>
2025-09-04 11:35:58 -0400

    fix r version issue

--- .lmod/setup_modules.sh
+++ .lmod/setup_modules.sh
@@ -11,7 +11,7 @@ module purge
 module load fsl/6.0.3
 module load freesurfer/6.0.0
 module load afni/20.3.00
-module load r/4.2.1
+module load r
 module load flywheel/16.4.0
 module load dcm2niix/1.0.20211006
 
